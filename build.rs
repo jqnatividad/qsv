1aa57c8da47c66ab4c837c0054731e8c2a5f825e
Joel Natividad <1980690+jqnatividad@users.noreply.github.com>
2021-09-29 11:46:04 -0400

    Check if python is installed before building
    Initial installment for #55.
    
    For now, let's stop the build if python is not installed.
    
    Will improve later so we actually can do conditional compilation of `py` command only when Python is installed.
    
    Also, instead of just warning qsv requires >=  rust 1.50, actually stop the build.

--- build.rs
+++ build.rs
@@ -1,9 +1,24 @@
 extern crate rustc_version;
 
+use std::process::{Command,exit};
 use rustc_version::{version, Version};  
 
 fn main() {
-    if version().unwrap() >= Version::parse("1.50.0").unwrap() {
+    if version().unwrap() < Version::parse("1.50.0").unwrap() {
         println!("qsv requires rustc >= 1.50.0.");
-    }   
+        exit(1);
+    }
+    
+    let _output = if cfg!(target_os = "windows") {
+        Command::new("cmd")
+                .args(["/C", "python --version"])
+                .output()
+                .expect("Python required.")
+    } else {
+        Command::new("sh")
+                .arg("-c")
+                .arg("python --version")
+                .output()
+                .expect("Python required.")
+    };
 }
