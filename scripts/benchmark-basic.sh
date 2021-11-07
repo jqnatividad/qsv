5f69cfebea4fb3d8b5f23f24a53e191126804c6e
Joel Natividad <1980690+jqnatividad@users.noreply.github.com>
2021-11-07 14:20:48 -0500

    Revamped benchmark script
    - now only uses NYC 311 data
    - much larger benchmark data
      - from 48mb, 7 column, 1M row file, to 514 mb 514mb, 48 column, 1M row file

--- scripts/benchmark-basic.sh
+++ scripts/benchmark-basic.sh
@@ -1,8 +1,7 @@
 #!/bin/bash
 
-# This script does some very basic benchmarks with 'qsv' using a 1M row 
-# city population data set, and a 100k row sample of NYC's 311 data. If these
-# doesn't exist on your system, it will be downloaded for you.
+# This script does some very basic benchmarks with 'qsv' using a 520mb, 1M row sample of 
+# NYC's 311 data. If it doesn't exist on your system, it will be downloaded for you.
 #
 # These aren't meant to be overly rigorous, but they should be enough to catch
 # significant regressions.
@@ -19,33 +18,25 @@ set -e
 pat="$1"
 # you can change bin_name to another binary, like xsv
 bin_name=qsv
-datazip=worldcitiespop_mil.zip
-data=worldcitiespop_mil.csv
-countrydata=countrynames.csv
-data_idx=worldcitiespop_mil.csv.idx
+datazip=/tmp/NYC_311_SR_2010-2020-sample-1M.7z
+data=NYC_311_SR_2010-2020-sample-1M.csv
+commboarddata=communityboards.csv
+data_idx=NYC_311_SR_2010-2020-sample-1M.csv.idx
 data_to_exclude=data_to_exclude.csv
 searchset_patterns=searchset_patterns.txt
-nyc311datazip=nyc-311-sample-100k.zip
-nyc311data=nyc-311-sample-100k.csv
 if [ ! -r "$data" ]; then
   printf "Downloading benchmarking data...\n"
-  curl -sS https://raw.githubusercontent.com/wiki/jqnatividad/qsv/files/worldcitiespop_mil.zip > "$datazip"
-  unzip "$datazip"
-  "$bin_name" sample --seed 42 50000 "$data" -o "$data_to_exclude"
-  printf "santa\nfort\ncamp\n" > "$searchset_patterns"
-  curl -sS https://raw.githubusercontent.com/wiki/jqnatividad/qsv/files/nyc-311-sample-100k.zip > "$nyc311datazip"
-  unzip "$nyc311datazip"
+  curl -sS https://raw.githubusercontent.com/wiki/jqnatividad/qsv/files/NYC_311_SR_2010-2020-sample-1M.7z > "$datazip"
+  p7zip -d "$datazip"
+  "$bin_name" sample --seed 42 1000 "$data" -o "$data_to_exclude"
+  printf "homeless\npark\nnoise\n" > "$searchset_patterns"
+  curl -sS https://raw.githubusercontent.com/wiki/jqnatividad/qsv/files/communityboards.csv > "$commboarddata"
 fi
 os_type=$(echo $OSTYPE | cut -c 1-6)
 if [[ "$os_type" == "darwin" ]]; then
   data_size=$(stat -f '%z' "$data")
-  nyc311data_size=$(stat -f '%z' "$nyc311data")
 else
   data_size=$(stat --format '%s' "$data")
-  nyc311data_size=$(stat --format '%s' "$nyc311data")
-fi
-if [ ! -r "$countydata" ]; then
-  curl -sS https://gist.githubusercontent.com/anonymous/063cb470e56e64e98cf1/raw/98e2589b801f6ca3ff900b01a87fbb7452eb35c7/countrynames.csv > "$countrydata"
 fi
 
 function real_seconds {
@@ -82,11 +73,9 @@ function benchmark_with_index {
 
 function run {
   index=
-  nyc311=
   while true; do
     case "$1" in
       --index) index="yes" && shift ;;
-      --nyc311) nyc311="yes" && shift ;;
       *) break ;;
     esac
   done
@@ -100,13 +89,8 @@ function run {
     else
       t=$(benchmark_with_index "$@")
     fi
-    if [ -z "$nyc311" ]; then
-      mb_per=$(echo "scale=2; ($data_size / $t) / 2^20" | bc)
-      recs_per=$(echo "scale=2; (1000000 / $t)" | bc)
-    else
-      mb_per=$(echo "scale=2; ($nyc311data_size / $t) / 2^20" | bc)
-      recs_per=$(echo "scale=2; (100000 / $t)" | bc)
-    fi
+    mb_per=$(echo "scale=2; ($data_size / $t) / 2^20" | bc)
+    recs_per=$(echo "scale=2; (1000000 / $t)" | bc)
     mb_per=$(printf "%0.02f" $mb_per)
     recs_per=$(printf "%'.2f" $recs_per)
     printf -v tprint "%0.02f" $t
@@ -117,28 +101,24 @@ function run {
 
 binver=$("$bin_name" --version)
 current_time=$(date "+%Y-%m-%d-%H-%M-%S")
-# printf "Scrambling benchmark data...\n"
-# "$bin_name" index "$data"
-# "$bin_name" scramble "$data" > temp.csv
-# mv -f temp.csv "$data"
 benchmarkfile=$bin_name-bench-$binver-$current_time.tsv
 printf "%-27s%-11s%-12s%-12s\n" BENCHMARK TIME_SECS MB_PER_SEC RECS_PER_SEC
 printf "benchmark\ttime_secs\tmb_per_sec\trecs_per_sec\n" > $benchmarkfile
-run apply_op_string "$bin_name" apply operations lower Country -q "$data"
-run apply_op_string_ascii "$bin_name" apply operations asciilower Country -q "$data"
-run apply_op_similarity "$bin_name" apply operations lower,simdln Country --comparand union --new-column Country_sim-union_score -q "$data"
-run apply_op_soundex "$bin_name" apply operations lower,soundex City --comparand boston --new-column City_boston_soundex -q "$data" 
-run --nyc311 apply_datefmt "$bin_name" apply datefmt \"Created Date\" -q "$nyc311data"
-run --nyc311 apply_emptyreplace "$bin_name" apply emptyreplace \"Bridge Highway Name\" --replacement Unspecified -q "$nyc311data"
-run --nyc311 apply_geocode "$bin_name" apply geocode Location --new-column geocoded_location -q "$nyc311data"
+run apply_op_string "$bin_name" apply operations lower Agency -q "$data"
+run apply_op_string_ascii "$bin_name" apply operations asciilower Agency -q "$data"
+run apply_op_similarity "$bin_name" apply operations lower,simdln Agency --comparand brooklyn --new-column Agency_sim-brooklyn_score -q "$data"
+run apply_op_soundex "$bin_name" apply operations lower,soundex Agency --comparand Queens --new-column Agency_queens_soundex -q "$data" 
+run apply_datefmt "$bin_name" apply datefmt \"Created Date\" -q "$data"
+run apply_emptyreplace "$bin_name" apply emptyreplace \"Bridge Highway Name\" --replacement Unspecified -q "$data"
+run apply_geocode "$bin_name" apply geocode Location --new-column geocoded_location -q "$data"
 run count "$bin_name" count "$data"
 run --index count_index "$bin_name" count "$data"
 run dedup "$bin_name" dedup "$data"
 run enum "$bin_name" enum "$data"
-run exclude "$bin_name" exclude Country "$data" Country "$data_to_exclude"
-run --index exclude_index "$bin_name" exclude Country "$data" Country "$data_to_exclude"
+run exclude "$bin_name" exclude 'Incident Zip' "$data" 'Incident Zip' "$data_to_exclude"
+run --index exclude_index "$bin_name" exclude 'Incident Zip' "$data" 'Incident Zip' "$data_to_exclude"
 run explode "$bin_name" explode City "-" "$data"
-run fill "$bin_name" fill -v Unknown Population "$data"
+run fill "$bin_name" fill -v Unspecified 'Address Type' "$data"
 run fixlengths "$bin_name" fixlengths "$data"
 run flatten "$bin_name" flatten "$data"
 run flatten_condensed "$bin_name" flatten "$data" --condense 50
@@ -148,11 +128,11 @@ run --index frequency_index "$bin_name" frequency "$data"
 run frequency_selregex "$bin_name" frequency -s /^R/ "$data"
 run frequency_j1 "$bin_name" frequency -j 1 "$data"
 run index "$bin_name" index "$data"
-run join "$bin_name" join --no-case Country "$data" Abbrev "$countrydata"
-run lua "$bin_name" lua map pop_empty "tonumber\(Population\)==nil" -q "$data"
-run partition "$bin_name" partition Region /tmp/partitioned "$data"
-run --nyc311 pseudo "$bin_name" pseudo 'Unique Key' "$nyc311data"
-run rename "$bin_name" rename 'country,city,accent_city,region,population,lat,long' "$data"
+run join "$bin_name" join --no-case 'Community Board' "$data" community_board "$commboarddata"
+run lua "$bin_name" lua map location_empty "tonumber\(Location\)==nil" -q "$data"
+run partition "$bin_name" partition 'Community Board' /tmp/partitioned "$data"
+run pseudo "$bin_name" pseudo 'Unique Key' "$data"
+run rename "$bin_name" rename 'unique_key,created_date,closed_date,agency,agency_name,complaint_type,descriptor,loctype,zip,addr1,street,xstreet1,xstreet2,addrtype,city,landmark,facility_type,status,due_date,res_desc,res_act_date,comm_board,bbl,boro,xcoord,ycoord,opendata_type,parkname,parkboro,vehtype,taxi_boro,taxi_loc,bridge_hwy_name,bridge_hwy_dir,ramp,bridge_hwy_seg,lat,long,loc' "$data"
 run reverse "$bin_name" reverse "$data"
 run sample_10 "$bin_name" sample 10 "$data"
 run --index sample_10_index "$bin_name" sample 10 "$data"
@@ -160,15 +140,18 @@ run sample_1000 "$bin_name" sample 1000 "$data"
 run --index sample_1000_index "$bin_name" sample 1000 "$data"
 run sample_100000 "$bin_name" sample 100000 "$data"
 run --index sample_100000_index "$bin_name" sample 100000 "$data"
+run sample_100000_seeded "$bin_name" sample 100000 --seed 42 "$data"
+run --index sample_100000_seeded_index "$bin_name" sample --seed 42 100000 "$data"
 run --index sample_25pct_index "$bin_name" sample 0.25 "$data"
-run --index scramble_index "$bin_name" scramble -q "$data"
-run search "$bin_name" search -s Country "'(?i)us'" "$data"
+run --index sample_25pct_seeded_index "$bin_name" sample 0.25 --seed 42 "$data"
+run search "$bin_name" search -s 'Agency Name' "'(?i)us'" "$data"
 run searchset "$bin_name" searchset "$searchset_patterns" "$data"
-run select "$bin_name" select 'Country,City' "$data"
+run select "$bin_name" select 'Agency,Community Board' "$data"
 run select_regex "$bin_name" select /^L/ "$data"
 run slice_one_middle "$bin_name" slice -i 500000 "$data"
 run --index slice_one_middle_index "$bin_name" slice -i 500000 "$data"
-run sort "$bin_name" sort -s Country "$data"
+run sort "$bin_name" sort -s 'Incident Zip' "$data"
+run sort_random_seeded "$bin_name" sort --random --seed 42 "$data"
 run split "$bin_name" split --size 50000 split_tempdir "$data"
 run --index split_index "$bin_name" split --size 50000 split_tempdir "$data"
 run --index split_index_j1 "$bin_name" split --size 50000 -j 1 split_tempdir "$data"
