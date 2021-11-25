c21407c990176b0a1faedb9bba3a8d5dbdd4b8fb
Joel Natividad <1980690+jqnatividad@users.noreply.github.com>
2021-11-25 08:53:36 -0500

    rustfmt

--- tests/test_comments.rs
+++ tests/test_comments.rs
@@ -5,13 +5,23 @@ fn comments() {
     let wrk = Workdir::new("comments");
     wrk.create(
         "comments.csv",
-        vec![svec!["# test file to see how comments work",""], svec!["column1", "column2"], svec!["a", "1"], svec!["#b", "2"], svec!["c", "3"]],
+        vec![
+            svec!["# test file to see how comments work", ""],
+            svec!["column1", "column2"],
+            svec!["a", "1"],
+            svec!["#b", "2"],
+            svec!["c", "3"],
+        ],
     );
     let mut cmd = wrk.command("input");
     cmd.env("QSV_COMMENTS", "#");
     cmd.arg("comments.csv");
 
     let got: Vec<Vec<String>> = wrk.read_stdout(&mut cmd);
-    let expected = vec![svec!["column1", "column2"], svec!["a", "1"], svec!["c", "3"]];
+    let expected = vec![
+        svec!["column1", "column2"],
+        svec!["a", "1"],
+        svec!["c", "3"],
+    ];
     assert_eq!(got, expected);
 }
