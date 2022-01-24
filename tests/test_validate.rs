e785257b4fe3e6e4b3a48e340143055c5230255e
Michael S. Huang <mhuang74@gmail.com>
2022-01-24 19:40:17 +0800

    enhanced integ test; add detailed example

--- tests/test_validate.rs
+++ tests/test_validate.rs
@@ -37,7 +37,7 @@ fn validate_bad_csv() {
 }
 
 #[test]
-fn validate_with_json_schema() {
+fn validate_adur_public_toilets_dataset_with_json_schema() {
     // Public Toilets Schema
     // manually converted from JSON Table Schema
     // https://github.com/esd-org-uk/schemas/blob/master/PublicToilets/PublicToilets.json
@@ -49,177 +49,179 @@ fn validate_with_json_schema() {
         "description": "List of Public Toilets",
         "type": "object",
         "properties": {
-          "ExtractDate":
-          {
+            "ExtractDate":
+            {
             "description": "The date that the data was last extracted from its source database or manually updated",
             "type": ["string"],
-            "pattern": "\\d{2}\\/\\d{2}\\/\\d{4} 00:00"
-           },
-          "OrganisationURI":
-          {
+            "pattern": "(^([0-2]\\d|[3][0-1])\\/([0]\\d|[1][0-2])\\/[1-2]\\d{3}((\\s([0-1]\\d|[2][0-3]):[0-5]\\d(:[0-5]\\d)?)?)|)$"
+            },
+            "OrganisationURI":
+            {
             "description": "URI from ODC of publishing organisation",
             "type": ["string", "null"]
-          },
-          "OrganisationLabel":
-          {
+            },
+            "OrganisationLabel":
+            {
             "description": "Label of publishing organisation. If empty uses this will be the same as the publishing organisation",
             "type": ["string"]
-          },
-          "ServiceTypeURI":
-          {
+            },
+            "ServiceTypeURI":
+            {
             "description": "Service URI from http://id.esd.org.uk/list/services",
             "type": ["string", "null"]
-          },
-          "ServiceTypeLabel":
-          {
+            },
+            "ServiceTypeLabel":
+            {
             "description": "Label of the Service Type URI",
             "type": ["string"]
-          },
-          "LocationText":
-          {
+            },
+            "LocationText":
+            {
             "description": "Name of building, street, park etc.",
             "type": ["string"]
-          },
-          "CoordinateReferenceSystem":
-          {
+            },
+            "CoordinateReferenceSystem":
+            {
             "description": "‘WGS84’ (http://en.wikipedia.org/wiki/WGS84) or ‘OSGB36’ (http://en.wikipedia.org/wiki/Ordnance_Survey_National_Grid). WGS84 is the default.",
-            "type": ["string", "null"]
-          },
-          "GeoX":
-          {
+            "type": ["string", "null"],
+            "pattern": "(WGS84|OSGB36)"
+            },
+            "GeoX":
+            {
             "description": "Longitude or east grid reference for centroid of application boundary",
             "type": "number"
-          },
-          "GeoY":
-          {
+            },
+            "GeoY":
+            {
             "description": "Latitude or north grid reference for centroid of application boundary",
             "type": "number"
-          },
-          "GeoPointLicensingURL":
-          {
+            },
+            "GeoPointLicensingURL":
+            {
             "description": "URL of any page that describes any licensing restrictions on using the northing and easting.",
             "type": ["string", "null"]
-          },
-          "Category":
-          {
+            },
+            "Category":
+            {
             "description": "Use `None` if only accessible",
-            "type": ["string", "null"]
-          },
-          "AccessibleCategory":
-          {
+            "type": ["string", "null"],
+            "pattern": "(Female|Male|Female and Male|Unisex|Male urinal|Children only|None)"
+            },
+            "AccessibleCategory":
+            {
             "description": "Use `None` if not accessible",
             "type": ["string", "null"]
-          },
-          "RADARKeyNeeded":
-          {
+            },
+            "RADARKeyNeeded":
+            {
             "description": "Flag to show whether a RADAR Key is needed",
             "type": ["string", "null"]
-          },
-          "BabyChange":
-          {
+            },
+            "BabyChange":
+            {
             "description": "Flag to show whether there are baby changing facilities",
             "type": ["string", "null"]
-          },
-          "FamilyToilet":
-          {
+            },
+            "FamilyToilet":
+            {
             "description": "Flag to show whether there is a family toilet",
             "type": ["string", "null"]
-          },
-          "ChangingPlace":
-          {
+            },
+            "ChangingPlace":
+            {
             "description": "Flag to indicate whether the toilet includes changing facilities for people with profound and multiple learning disabilities and their carers, as well as many other disabled people. See http://www.changing-places.org/",
             "type": ["string", "null"]
-          },
-          "AutomaticPublicConvenience":
-          {
+            },
+            "AutomaticPublicConvenience":
+            {
             "description": "Flag to indicate whether this is an automatic public convenience (aka superloo)",
             "type": ["string", "null"]
-          },
-          "FullTimeStaffing":
-          {
+            },
+            "FullTimeStaffing":
+            {
             "description": "Flag to indicate whether there is full time staffing",
             "type": ["string", "null"]
-          },
-          "PartOfCommunityScheme":
-          {
+            },
+            "PartOfCommunityScheme":
+            {
             "description": "Flag to indicate whether it is part of a community scheme",
             "type": ["string", "null"]
-          },
-          "CommunitySchemeName":
-          {
+            },
+            "CommunitySchemeName":
+            {
             "description": "The name of the community scheme",
             "type": ["string", "null"]
-          },
-          "ChargeAmount":
-          {
+            },
+            "ChargeAmount":
+            {
             "description": "Charge amount in GBP to two decimal places. Omit the currency symbol",
             "type": ["number", "null"]
-          },
-          "InfoURL":
-          {
+            },
+            "InfoURL":
+            {
             "description": "URL of web page describing the facilities",
             "type": ["string", "null"]
-          },
-          "OpeningHours":
-          {
+            },
+            "OpeningHours":
+            {
             "description": "Opening Hours",
             "type": ["string", "null"]
-          },
-          "ManagedBy":
-          {
+            },
+            "ManagedBy":
+            {
             "description": "Name of organisation managing the toilet",
             "type": ["string", "null"]
-          },
-          "ReportEmail":
-          {
+            },
+            "ReportEmail":
+            {
             "description": "Email address for reporting problems",
             "type": ["string", "null"]
-          },
-          "ReportTel":
-          {
+            },
+            "ReportTel":
+            {
             "description": "Telephone number for reporting problems",
             "type": ["string", "null"]
-          },
-          "Notes":
-          {
+            },
+            "Notes":
+            {
             "description": "Notes on reporting, accesibility, hours or anything else",
             "type": ["string", "null"]
-          },
-          "UPRN":
-          {
+            },
+            "UPRN":
+            {
             "description": "Unique Property Reference Number of the toilet",
             "type": ["string", "null"]
-          },
-          "Postcode":
-          {
+            },
+            "Postcode":
+            {
             "description": "Postcode of the toilet",
             "type": ["string", "null"]
-          },
-          "StreetAddress":
-          {
+            },
+            "StreetAddress":
+            {
             "description": "Street address of the toilet",
             "type": ["string", "null"]
-          },
-          "GeoAreaURI":
-          {
+            },
+            "GeoAreaURI":
+            {
             "description": "A predefined spatial area that the application is contained in.  Note that these can be derived from X/Y co-ordinates. Comma delimit in spreadsheet where there is more than one",
             "type": ["string", "null"]
-          },
-          "GeoAreaLabel":
-          {
+            },
+            "GeoAreaLabel":
+            {
             "description": "Labels of the Geo Area URI. Comma delimit in spreadsheet where there is more than one",
             "type": ["string", "null"]
-          }
+            }
         },
         "required": [ "ExtractDate", "OrganisationLabel", "ServiceTypeLabel", "LocationText", "GeoX", "GeoY" ]
-      }
+        }
     "#;
 
     let csv = 
 r#"ExtractDate,OrganisationURI,OrganisationLabel,ServiceTypeURI,ServiceTypeLabel,LocationText,CoordinateReferenceSystem,GeoX,GeoY,GeoPointLicensingURL,Category,AccessibleCategory,RADARKeyNeeded,BabyChange,FamilyToilet,ChangingPlace,AutomaticPublicConvenience,FullTimeStaffing,PartOfCommunityScheme,CommunitySchemeName,ChargeAmount,InfoURL,OpeningHours,ManagedBy,ReportEmail,ReportTel,Notes,UPRN,Postcode,StreetAddress,GeoAreaURI,GeoAreaLabel
- ,http://opendatacommunities.org/id/district-council/adur,,http://id.esd.org.uk/service/579,Public toilets,BEACH GREEN PUBLIC CONVENIENCES BRIGHTON ROAD LANCING,OSGB36,518072,103649,http://www.ordnancesurvey.co.uk/business-and-government/help-and-support/public-sector/guidance/derived-data-exemptions.html,Female and male,Unisex,Yes,No,No,No,No,No,No,,,http://www.adur-worthing.gov.uk/streets-and-travel/public-toilets/,"S = 09:00 - 21:00 W = 09:00 - 17:00 ",ADC,surveyors@adur-worthing.gov.uk,01903 221471,,60001449,,BEACH GREEN PUBLIC CONVENIENCES BRIGHTON ROAD LANCING,,
+,http://opendatacommunities.org/id/district-council/adur,,http://id.esd.org.uk/service/579,Public toilets,BEACH GREEN PUBLIC CONVENIENCES BRIGHTON ROAD LANCING,OSGB36,518072,103649,http://www.ordnancesurvey.co.uk/business-and-government/help-and-support/public-sector/guidance/derived-data-exemptions.html,Female and male,Unisex,Yes,No,No,No,No,No,No,,,http://www.adur-worthing.gov.uk/streets-and-travel/public-toilets/,"S = 09:00 - 21:00 W = 09:00 - 17:00 ",ADC,surveyors@adur-worthing.gov.uk,01903 221471,,60001449,,BEACH GREEN PUBLIC CONVENIENCES BRIGHTON ROAD LANCING,,
 07/07/2014 00:00,http://opendatacommunities.org/id/district-council/adur,Adur,http://id.esd.org.uk/service/579,Public toilets,PUBLIC CONVENIENCES MONKS RECREATION GROUND CRABTREE LANE LANCING,OSGB36,518225,104730,http://www.ordnancesurvey.co.uk/business-and-government/help-and-support/public-sector/guidance/derived-data-exemptions.html,Female and male,None,Yes,No,No,No,No,No,No,,,http://www.adur-worthing.gov.uk/streets-and-travel/public-toilets/,"S = 09:00 - 15:00 W = 09:00 - 15:00",ADC,surveyors@adur-worthing.gov.uk,01903 221471,,60002210,,PUBLIC CONVENIENCES MONKS RECREATION GROUND CRABTREE LANE LANCING,,
-2014-07-07 00:00,http://opendatacommunities.org/id/district-council/adur,Adur,http://id.esd.org.uk/service/579,Public toilets,PUBLIC CONVENIENCES SHOPSDAM ROAD LANCING,OSGB36,518915,103795,http://www.ordnancesurvey.co.uk/business-and-government/help-and-support/public-sector/guidance/derived-data-exemptions.html,Female and male,Unisex,Yes,No,No,No,No,No,No,,,http://www.adur-worthing.gov.uk/streets-and-travel/public-toilets/,"S = 09:00 - 21:00 W = 09:00 - 17:00",ADC,surveyors@adur-worthing.gov.uk,01903 221471,,60007428,,PUBLIC CONVENIENCES SHOPSDAM ROAD LANCING,,
+2014-07-07 00:00,http://opendatacommunities.org/id/district-council/adur,Adur,http://id.esd.org.uk/service/579,Public toilets,PUBLIC CONVENIENCES SHOPSDAM ROAD LANCING,OSGB3,518915,103795,http://www.ordnancesurvey.co.uk/business-and-government/help-and-support/public-sector/guidance/derived-data-exemptions.html,Mens,Unisex,Yes,No,No,No,No,No,No,,,http://www.adur-worthing.gov.uk/streets-and-travel/public-toilets/,"S = 09:00 - 21:00 W = 09:00 - 17:00",ADC,surveyors@adur-worthing.gov.uk,01903 221471,,60007428,,PUBLIC CONVENIENCES SHOPSDAM ROAD LANCING,,
 07/07/2014 00:00,http://opendatacommunities.org/id/district-council/adur,Adur,http://id.esd.org.uk/service/579,Public toilets,PUBLIC CONVENIENCES YEW TREE CLOSE LANCING,OSGB36,518222,104168,http://www.ordnancesurvey.co.uk/business-and-government/help-and-support/public-sector/guidance/derived-data-exemptions.html,Female and male,Unisex,Yes,No,No,No,No,No,No,,,http://www.adur-worthing.gov.uk/streets-and-travel/public-toilets/,"S = 09:00 - 21:00 W = 09:00 - 17:00",ADC,surveyors@adur-worthing.gov.uk,01903 221471,,60008859,,PUBLIC CONVENIENCES YEW TREE CLOSE LANCING,,
 07/07/2014 00:00,http://opendatacommunities.org/id/district-council/adur,Adur,http://id.esd.org.uk/service/579,Public toilets,PUBLIC CONVENIENCES BEACH GREEN SHOREHAM-BY-SEA,OSGB36,521299,104515,http://www.ordnancesurvey.co.uk/business-and-government/help-and-support/public-sector/guidance/derived-data-exemptions.html,Female and male,Unisex,Yes,No,No,No,No,No,No,,,http://www.adur-worthing.gov.uk/streets-and-travel/public-toilets/,"S = 09:00 - 21:00 W = 09:00 - 17:00",ADC,surveyors@adur-worthing.gov.uk,01903 221471,,60009402,,PUBLIC CONVENIENCES BEACH GREEN SHOREHAM-BY-SEA,,
 07/07/2014 00:00,http://opendatacommunities.org/id/district-council/adur,Adur,http://id.esd.org.uk/service/579,Public toilets,PUBLIC CONVENIENCES ADUR RECREATION GROUND BRIGHTON ROAD SHOREHAM-BY-SEA,OSGB36,521048,104977,http://www.ordnancesurvey.co.uk/business-and-government/help-and-support/public-sector/guidance/derived-data-exemptions.html,Female and male,Unisex,Yes,No,No,No,No,No,No,,,http://www.adur-worthing.gov.uk/streets-and-travel/public-toilets/,"S = 08:00 - 21:00 W = 08:00 - 17:00",ADC,surveyors@adur-worthing.gov.uk,01903 221471,,60009666,,PUBLIC CONVENIENCES ADUR RECREATION GROUND BRIGHTON ROAD SHOREHAM-BY-SEA,,
@@ -236,18 +238,18 @@ r#"ExtractDate,OrganisationURI,OrganisationLabel,ServiceTypeURI,ServiceTypeLabel
 
     // invalid records with index from original csv
     // row 1: missing values for ExtactDate and OrganisationLabel
-    // row 3: wrong date format for ExtractDate
+    // row 3: wrong value for CoordinateReferenceSystem and Category
     // note: removed unnecessary quotes for string column "OpeningHours"
     let invalid_expected =
 r#"ExtractDate,OrganisationURI,OrganisationLabel,ServiceTypeURI,ServiceTypeLabel,LocationText,CoordinateReferenceSystem,GeoX,GeoY,GeoPointLicensingURL,Category,AccessibleCategory,RADARKeyNeeded,BabyChange,FamilyToilet,ChangingPlace,AutomaticPublicConvenience,FullTimeStaffing,PartOfCommunityScheme,CommunitySchemeName,ChargeAmount,InfoURL,OpeningHours,ManagedBy,ReportEmail,ReportTel,Notes,UPRN,Postcode,StreetAddress,GeoAreaURI,GeoAreaLabel
- ,http://opendatacommunities.org/id/district-council/adur,,http://id.esd.org.uk/service/579,Public toilets,BEACH GREEN PUBLIC CONVENIENCES BRIGHTON ROAD LANCING,OSGB36,518072,103649,http://www.ordnancesurvey.co.uk/business-and-government/help-and-support/public-sector/guidance/derived-data-exemptions.html,Female and male,Unisex,Yes,No,No,No,No,No,No,,,http://www.adur-worthing.gov.uk/streets-and-travel/public-toilets/,S = 09:00 - 21:00 W = 09:00 - 17:00 ,ADC,surveyors@adur-worthing.gov.uk,01903 221471,,60001449,,BEACH GREEN PUBLIC CONVENIENCES BRIGHTON ROAD LANCING,,
-2014-07-07 00:00,http://opendatacommunities.org/id/district-council/adur,Adur,http://id.esd.org.uk/service/579,Public toilets,PUBLIC CONVENIENCES SHOPSDAM ROAD LANCING,OSGB36,518915,103795,http://www.ordnancesurvey.co.uk/business-and-government/help-and-support/public-sector/guidance/derived-data-exemptions.html,Female and male,Unisex,Yes,No,No,No,No,No,No,,,http://www.adur-worthing.gov.uk/streets-and-travel/public-toilets/,S = 09:00 - 21:00 W = 09:00 - 17:00,ADC,surveyors@adur-worthing.gov.uk,01903 221471,,60007428,,PUBLIC CONVENIENCES SHOPSDAM ROAD LANCING,,
+,http://opendatacommunities.org/id/district-council/adur,,http://id.esd.org.uk/service/579,Public toilets,BEACH GREEN PUBLIC CONVENIENCES BRIGHTON ROAD LANCING,OSGB36,518072,103649,http://www.ordnancesurvey.co.uk/business-and-government/help-and-support/public-sector/guidance/derived-data-exemptions.html,Female and male,Unisex,Yes,No,No,No,No,No,No,,,http://www.adur-worthing.gov.uk/streets-and-travel/public-toilets/,S = 09:00 - 21:00 W = 09:00 - 17:00 ,ADC,surveyors@adur-worthing.gov.uk,01903 221471,,60001449,,BEACH GREEN PUBLIC CONVENIENCES BRIGHTON ROAD LANCING,,
+2014-07-07 00:00,http://opendatacommunities.org/id/district-council/adur,Adur,http://id.esd.org.uk/service/579,Public toilets,PUBLIC CONVENIENCES SHOPSDAM ROAD LANCING,OSGB3,518915,103795,http://www.ordnancesurvey.co.uk/business-and-government/help-and-support/public-sector/guidance/derived-data-exemptions.html,Mens,Unisex,Yes,No,No,No,No,No,No,,,http://www.adur-worthing.gov.uk/streets-and-travel/public-toilets/,S = 09:00 - 21:00 W = 09:00 - 17:00,ADC,surveyors@adur-worthing.gov.uk,01903 221471,,60007428,,PUBLIC CONVENIENCES SHOPSDAM ROAD LANCING,,
 "#;
 
     // validation report
     let validation_errors_expected = 
 r#"{"valid":false,"errors":[{"keywordLocation":"/properties/ExtractDate/type","instanceLocation":"/ExtractDate","absoluteKeywordLocation":"https://example.com/properties/ExtractDate/type","error":"null is not of type \"string\""},{"keywordLocation":"/properties/OrganisationLabel/type","instanceLocation":"/OrganisationLabel","absoluteKeywordLocation":"https://example.com/properties/OrganisationLabel/type","error":"null is not of type \"string\""}],"row_index":1}
-{"valid":false,"errors":[{"keywordLocation":"/properties/ExtractDate/pattern","instanceLocation":"/ExtractDate","absoluteKeywordLocation":"https://example.com/properties/ExtractDate/pattern","error":"\"2014-07-07 00:00\" does not match \"\\d{2}\\/\\d{2}\\/\\d{4} 00:00\""}],"row_index":3}
+{"valid":false,"errors":[{"keywordLocation":"/properties/CoordinateReferenceSystem/pattern","instanceLocation":"/CoordinateReferenceSystem","absoluteKeywordLocation":"https://example.com/properties/CoordinateReferenceSystem/pattern","error":"\"OSGB3\" does not match \"(WGS84|OSGB36)\""},{"keywordLocation":"/properties/Category/pattern","instanceLocation":"/Category","absoluteKeywordLocation":"https://example.com/properties/Category/pattern","error":"\"Mens\" does not match \"(Female|Male|Female and Male|Unisex|Male urinal|Children only|None)\""}],"row_index":3}
 "#;
 
     let wrk = Workdir::new("validate").flexible(true);
