f45b332d655d26994488cc5f499696379209b5ad
Joel Natividad <1980690+jqnatividad@users.noreply.github.com>
2022-03-20 13:56:24 -0400

    Reformat `sniff` metadata display
    - removed "Dialect:" header
    - show "tab" if tab-delimited, otherwise, its invisible.
    
    improved `sniff` and `QSV_SNIFF_DELIMITER` tests

--- tests/test_sniff.rs
+++ tests/test_sniff.rs
@@ -1,64 +1,71 @@
 use crate::workdir::Workdir;
 
-#[test]
-fn sniff() {
-    let wrk = Workdir::new("sniff");
-    wrk.create(
-        "in.csv",
-        vec![
-            svec!["id", "timestamp", "h3"],
-            svec!["1", "2021-04-26 00:02:18", "a"],
-            svec!["2", "2021-04-26 19:22:26", "b"],
-            svec!["30", "2021-04-26 11:44:13", "c"],
-            svec!["4", "2021-04-26 14:37:03", "d"],
-            svec!["2", "2021-04-26 20:22:26", "e"],
-            svec!["5", "2021-04-26 19:29:26", "f"],
-            svec!["60", "2021-04-26 04:52:46", "g"],
-            svec!["2", "2021-04-26 19:12:26", "h"],
-            svec!["30", "2021-04-26 10:44:13", "i"],
-            svec!["30", "2021-04-26 09:44:13", "j"],
-            svec!["1", "2021-04-26 01:02:18", "k"],
-        ],
-    );
-
-    let mut cmd = wrk.command("sniff");
-    cmd.arg("in.csv");
-
-    let got: String = wrk.stdout(&mut cmd);
-
-    let expected = r#"Metadata
-========
-Dialect:
-	Delimiter: ,
-	Has header row?: true
-	Number of preamble rows: 0
-	Quote character: none
-	Double-quote escapes?: true
-	Escape character: none
-	Comment character: none
-	Flexible: false
-
-Number of fields: 3
-Types:
-	0: Unsigned
-	1: Text
-	2: Text"#;
-
-    assert_eq!(got, expected);
-}
-
 static EXPECTED_TABLE: &str = "\
-h1       h2   h3
-abcdefg  a    a
-a        abc  z\
+h1       h2  h3
+abcdefg  1   a
+a        2   z\
 ";
 
 fn data() -> Vec<Vec<String>> {
     vec![
         svec!["h1", "h2", "h3"],
-        svec!["abcdefg", "a", "a"],
-        svec!["a", "abc", "z"],
+        svec!["abcdefg", "1", "a"],
+        svec!["a", "2", "z"],
     ]
+}
+
+#[test]
+fn sniff() {
+    let wrk = Workdir::new("sniff");
+    wrk.create_with_delim("in.file", data(), b',');
+
+    let mut cmd = wrk.command("sniff");
+    cmd.arg("in.file");
+
+    let got: String = wrk.stdout(&mut cmd);
+
+    let expected = r#"Metadata
+========
+	Delimiter: ,
+	Has header row?: true
+	Number of preamble rows: 0
+	Quote character: none
+	Flexible: false
+
+Number of fields: 3
+Types:
+	0: Text
+	1: Unsigned
+	2: Text"#;
+
+    assert_eq!(got, expected);
+}
+
+#[test]
+fn sniff_tab() {
+    let wrk = Workdir::new("sniff_tab");
+    wrk.create_with_delim("in.file", data(), b'\t');
+
+    let mut cmd = wrk.command("sniff");
+    cmd.arg("in.file");
+
+    let got: String = wrk.stdout(&mut cmd);
+
+    let expected = r#"Metadata
+========
+	Delimiter: tab
+	Has header row?: true
+	Number of preamble rows: 0
+	Quote character: none
+	Flexible: false
+
+Number of fields: 3
+Types:
+	0: Text
+	1: Unsigned
+	2: Text"#;
+
+    assert_eq!(got, expected);
 }
 
 #[test]
