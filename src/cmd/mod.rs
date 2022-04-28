686ebcfaf3fcf68adff3d991639e95be674d85b7
Joel Natividad <1980690+jqnatividad@users.noreply.github.com>
2022-04-27 20:02:32 -0400

    conditional compilation of modules for qsvdp

--- src/cmd/mod.rs
+++ src/cmd/mod.rs
@@ -1,48 +1,68 @@
 #[cfg(all(feature = "apply", not(feature = "lite")))]
 pub mod apply;
+#[cfg(any(feature = "full", feature = "lite"))]
 pub mod behead;
+#[cfg(any(feature = "full", feature = "lite"))]
 pub mod cat;
 pub mod count;
 pub mod dedup;
+#[cfg(any(feature = "full", feature = "lite"))]
 pub mod enumerate;
 pub mod excel;
 pub mod exclude;
+#[cfg(any(feature = "full", feature = "lite"))]
 pub mod explode;
 #[cfg(all(feature = "fetch", not(feature = "lite")))]
 pub mod fetch;
+#[cfg(any(feature = "full", feature = "lite"))]
 pub mod fill;
+#[cfg(any(feature = "full", feature = "lite"))]
 pub mod fixlengths;
+#[cfg(any(feature = "full", feature = "lite"))]
 pub mod flatten;
+#[cfg(any(feature = "full", feature = "lite"))]
 pub mod fmt;
 #[cfg(all(feature = "foreach", target_family = "unix", not(feature = "lite")))]
 pub mod foreach;
 pub mod frequency;
 #[cfg(all(feature = "generate", not(feature = "lite")))]
 pub mod generate;
+#[cfg(any(feature = "full", feature = "lite"))]
 pub mod headers;
 pub mod index;
 pub mod input;
+#[cfg(any(feature = "full", feature = "lite"))]
 pub mod join;
+#[cfg(any(feature = "full", feature = "lite"))]
 pub mod jsonl;
 #[cfg(all(feature = "lua", not(feature = "lite")))]
 pub mod lua;
+#[cfg(any(feature = "full", feature = "lite"))]
 pub mod partition;
 pub mod pseudo;
 #[cfg(all(feature = "python", not(feature = "lite")))]
 pub mod python;
+#[cfg(any(feature = "full", feature = "lite"))]
 pub mod rename;
 pub mod replace;
+#[cfg(any(feature = "full", feature = "lite"))]
 pub mod reverse;
 pub mod sample;
+#[cfg(any(feature = "full", feature = "lite"))]
 pub mod schema;
 pub mod search;
 pub mod searchset;
 pub mod select;
 pub mod slice;
+#[cfg(any(feature = "full", feature = "lite"))]
 pub mod sniff;
 pub mod sort;
+#[cfg(any(feature = "full", feature = "lite"))]
 pub mod split;
 pub mod stats;
+#[cfg(any(feature = "full", feature = "lite"))]
 pub mod table;
+#[cfg(any(feature = "full", feature = "lite"))]
 pub mod transpose;
+#[cfg(any(feature = "full", feature = "lite"))]
 pub mod validate;
