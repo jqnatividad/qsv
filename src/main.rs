52f8182a158553064710b1a3f8a4e091e04af159
Joel Natividad <1980690+jqnatividad@users.noreply.github.com>
2022-05-01 12:52:37 -0400

    expand `input` description in main

--- src/main.rs
+++ src/main.rs
@@ -62,7 +62,7 @@ macro_rules! command_list {
     headers     Show header names
     help        Show this usage message
     index       Create CSV index for faster access
-    input       Read CSV data with special quoting, skipping & trimming rules
+    input       Read CSVs w/ special quoting, skipping, trimming & transcoding rules
     join        Join CSV files
     jsonl       Convert newline-delimited JSON files to CSV
     lua*        Execute Lua script on CSV data
