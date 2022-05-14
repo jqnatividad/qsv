a70da3ee5606f1a492ee16418cba25acb6216765
Joel Natividad <1980690+jqnatividad@users.noreply.github.com>
2022-05-14 01:11:15 -0400

    `dedup`: addl tests for testing `--sorted` option

--- tests/test_dedup.rs
+++ tests/test_dedup.rs
@@ -70,3 +70,152 @@ fn dedup_select() {
     let expected = vec![svec!["N", "S"], svec!["10", "a"], svec!["2", "B"]];
     assert_eq!(got, expected);
 }
+
+#[test]
+fn dedup_sorted() {
+    let wrk = Workdir::new("dedup_sorted");
+    wrk.create(
+        "in.csv",
+        vec![
+            svec!["N", "S"],
+            svec!["10", "a"],
+            svec!["10", "a"],
+            svec!["10", "b"],
+            svec!["20", "B"],
+            svec!["20", "b"],
+            svec!["3", "c"],
+            svec!["4", "d"],
+        ],
+    );
+
+    let mut cmd = wrk.command("dedup");
+    cmd.arg("--sorted").arg("in.csv");
+
+    let got: Vec<Vec<String>> = wrk.read_stdout(&mut cmd);
+    let expected = vec![
+        svec!["N", "S"],
+        svec!["10", "a"],
+        svec!["10", "b"],
+        svec!["20", "B"],
+        svec!["20", "b"],
+        svec!["3", "c"],
+        svec!["4", "d"],
+    ];
+    assert_eq!(got, expected);
+}
+
+#[test]
+fn dedup_sorted_nocase() {
+    let wrk = Workdir::new("dedup_sorted_nocase");
+    wrk.create(
+        "in.csv",
+        vec![
+            svec!["N", "S"],
+            svec!["10", "a"],
+            svec!["10", "A"],
+            svec!["10", "a"],
+            svec!["10", "A"],
+            svec!["11", "c"],
+            svec!["20", "b"],
+            svec!["20", "b"],
+            svec!["20", "B"],
+            svec!["20", "B"],
+            svec!["3", "c"],
+            svec!["4", "d"],
+        ],
+    );
+
+    let mut cmd = wrk.command("dedup");
+    cmd.arg("--sorted").arg("--no-case").arg("in.csv");
+
+    let got: Vec<Vec<String>> = wrk.read_stdout(&mut cmd);
+    let expected = vec![
+        svec!["N", "S"],
+        svec!["10", "a"],
+        svec!["11", "c"],
+        svec!["20", "b"],
+        svec!["3", "c"],
+        svec!["4", "d"],
+    ];
+    assert_eq!(got, expected);
+}
+
+#[test]
+fn dedup_alreadysorted_nocase() {
+    let wrk = Workdir::new("dedup_alreadysorted_nocase");
+    wrk.create(
+        "in.csv",
+        vec![
+            svec!["N", "S"],
+            svec!["10", "a"],
+            svec!["10", "a"],
+            svec!["10", "a"],
+            svec!["100", "a"],
+            svec!["100", "a"],
+            svec!["20", "b"],
+            svec!["20", "b"],
+            svec!["20", "B"],
+            svec!["20", "B"],
+            svec!["3", "c"],
+            svec!["4", "d"],
+        ],
+    );
+
+    let mut cmd = wrk.command("dedup");
+    cmd.arg("--no-case").arg("in.csv");
+
+    let got: Vec<Vec<String>> = wrk.read_stdout(&mut cmd);
+    let expected = vec![
+        svec!["N", "S"],
+        svec!["10", "a"],
+        svec!["100", "a"],
+        svec!["20", "b"],
+        svec!["3", "c"],
+        svec!["4", "d"],
+    ];
+    assert_eq!(got, expected);
+}
+
+#[test]
+fn dedup_not_sorted() {
+    let wrk = Workdir::new("dedup__not_sorted");
+    wrk.create(
+        "in.csv",
+        vec![
+            svec!["N", "S"],
+            svec!["30", "c"],
+            svec!["10", "a"],
+            svec!["10", "a"],
+            svec!["20", "b"],
+            svec!["20", "B"],
+        ],
+    );
+
+    let mut cmd = wrk.command("dedup");
+    cmd.arg("--sorted").arg("in.csv");
+
+    let got: String = wrk.output_stderr(&mut cmd);
+    assert!(got.contains("Aborting! Input not sorted!"));
+}
+
+#[test]
+fn dedup_not_sorted2() {
+    let wrk = Workdir::new("dedup__not_sorted2");
+    wrk.create(
+        "in.csv",
+        vec![
+            svec!["N", "S"],
+            svec!["10", "a"],
+            svec!["10", "a"],
+            svec!["20", "b"],
+            svec!["20", "B"],
+            svec!["1", "c"],
+        ],
+    );
+
+    let mut cmd = wrk.command("dedup");
+    cmd.arg("--sorted").arg("in.csv");
+
+    let got: String = wrk.output_stderr(&mut cmd);
+    assert!(got.contains("Aborting! Input not sorted!"));
+}
