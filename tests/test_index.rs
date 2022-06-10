d6a508d8770dd91de4c07a87de88f65a7b90a525
Joel Natividad <1980690+jqnatividad@users.noreply.github.com>
2022-06-09 21:21:29 -0400

    update tests for index
    count still works even if the index is stale
    other commands will fail if the index is stale

--- tests/test_index.rs
+++ tests/test_index.rs
@@ -5,9 +5,16 @@ use filetime::{set_file_times, FileTime};
 use crate::workdir::Workdir;
 
 #[test]
-fn index_outdated() {
-    let wrk = Workdir::new("index_outdated");
-    wrk.create_indexed("in.csv", vec![svec![""]]);
+fn index_outdated_count() {
+    let wrk = Workdir::new("index_outdated_count");
+    wrk.create_indexed(
+        "in.csv",
+        vec![
+            svec!["letter", "number"],
+            svec!["a", "13"],
+            svec!["b", "24"],
+        ],
+    );
 
     let md = fs::metadata(&wrk.path("in.csv.idx")).unwrap();
     set_file_times(
@@ -18,7 +25,40 @@ fn index_outdated() {
     .unwrap();
 
     let mut cmd = wrk.command("count");
-    cmd.arg("--no-headers").arg("in.csv");
+    cmd.arg("in.csv");
+
+    // count works even if index is stale
+    let expected_count = 2;
+    let got_count: usize = wrk.stdout(&mut cmd);
+    rassert_eq!(got_count, expected_count);
+}
+
+#[test]
+fn index_outdated_stats() {
+    let wrk = Workdir::new("index_outdated_stats");
+
+    wrk.create_indexed(
+        "in.csv",
+        vec![
+            svec!["letter", "number"],
+            svec!["a", "1"],
+            svec!["b", "2"],
+            svec!["c", "3"],
+        ],
+    );
+
+    let md = fs::metadata(&wrk.path("in.csv.idx")).unwrap();
+    set_file_times(
+        &wrk.path("in.csv"),
+        future_time(FileTime::from_last_modification_time(&md)),
+        future_time(FileTime::from_last_access_time(&md)),
+    )
+    .unwrap();
+
+    // stats should fail if the index is stale
+    let mut cmd = wrk.command("stats");
+    cmd.arg("in.csv");
+
     wrk.assert_err(&mut cmd);
 }
 
