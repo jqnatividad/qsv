3e85ecb192649539a5caf042e13f346c44a9282b
Joel Natividad <1980690+jqnatividad@users.noreply.github.com>
2022-11-03 19:57:08 -0400

    clippy::bool_to_int_with_if

--- tests/test_fill.rs
+++ tests/test_fill.rs
@@ -3,7 +3,7 @@ use crate::{workdir::Workdir, CsvRecord};
 fn compare_column(got: &[CsvRecord], expected: &[String], column: usize, skip_header: bool) {
     for (value, value_expected) in got
         .iter()
-        .skip(if skip_header { 1 } else { 0 })
+        .skip(usize::from(skip_header))
         .map(|row| &row[column])
         .zip(expected.iter())
     {
