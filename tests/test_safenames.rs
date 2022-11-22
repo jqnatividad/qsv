6fa4c7261ee1796b8c195b73fa2f0221d473da7c
Joel Natividad <1980690+jqnatividad@users.noreply.github.com>
2022-11-22 08:09:35 -0500

    add more tests/comments to test_safenames.rs

--- tests/test_safenames.rs
+++ tests/test_safenames.rs
@@ -13,10 +13,12 @@ fn safenames() {
                 "this is already a postgres safe column",
                 "1starts with 1",
                 "col1",
-                "col1"
+                "col1",
+                "",
+                "",
             ],
-            svec!["1", "b", "33", "1", "b", "33", "34"],
-            svec!["2", "c", "34", "3", "d", "31", "3"],
+            svec!["1", "b", "33", "1", "b", "33", "34", "z", "42"],
+            svec!["2", "c", "34", "3", "d", "31", "3", "y", "3.14"],
         ],
     );
 
@@ -28,14 +30,19 @@ fn safenames() {
         svec![
             "col1",
             "This_is_a_column_with_invalid_chars___and_leading___trailing",
+            // null column names are not allowed in postgres
             "_",
             "this is already a postgres safe column",
+            // a column cannot start with a digit
             "_starts_with_1",
+            // duplicate cols are not allowed in one table in postgres
             "col1_2",
-            "col1_3"
+            "col1_3",
+            "__2",
+            "__3"
         ],
-        svec!["1", "b", "33", "1", "b", "33", "34"],
-        svec!["2", "c", "34", "3", "d", "31", "3"],
+        svec!["1", "b", "33", "1", "b", "33", "34", "z", "42"],
+        svec!["2", "c", "34", "3", "d", "31", "3", "y", "3.14"],
     ];
     assert_eq!(got, expected);
 
@@ -52,8 +59,10 @@ fn safenames_always() {
         vec![
             svec![
                 "col1",
+                // not valid in postgres
                 " This is a column with invalid chars!# and leading & trailing spaces ",
                 "",
+                // postgres allows for embedded spaces
                 "this is already a postgres safe column",
                 "1starts with 1",
                 "col1",
@@ -73,6 +82,9 @@ fn safenames_always() {
             "col1",
             "This_is_a_column_with_invalid_chars___and_leading___trailing",
             "_",
+            // we were using Always mode, so even though the
+            // original header name was already valid,
+            // we replaced spaces with _ regardless
             "this_is_already_a_postgres_safe_column",
             "_starts_with_1",
             "col1_2",
@@ -112,6 +124,8 @@ fn safenames_ignore_invalid_mode() {
     cmd.arg("--mode").arg("invalidmode").arg("in.csv");
 
     let got: Vec<Vec<String>> = wrk.read_stdout(&mut cmd);
+
+    // if the mode is invalid, its a noop and we do not modify the header names
     let expected = vec![
         svec![
             "col1",
