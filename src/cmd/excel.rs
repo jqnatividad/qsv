9001cbf94fea184aefef80eb3b4f53429da79466
Joel Natividad <1980690+jqnatividad@users.noreply.github.com>
2022-12-15 09:07:44 -0500

    update other commands that use safe_header_names helper fn
    and just pass an empty vec for the reserved_names parameter

--- src/cmd/excel.rs
+++ src/cmd/excel.rs
@@ -231,7 +231,7 @@ pub fn run(argv: &[&str]) -> CliResult<()> {
                         .map(|h| {
                             let header = h.to_string();
 
-                            let safe_flag = util::is_safe_name(&header);
+                            let safe_flag = util::is_safe_name(&header, &Vec::new());
                             if safe_flag {
                                 if !safenames_vec.contains(&header) {
                                     safenames_vec.push(header.to_string());
@@ -474,8 +474,8 @@ pub fn run(argv: &[&str]) -> CliResult<()> {
                 DataType::Error(ref e) => record.push_field(&format!("{e:?}")),
                 DataType::Bool(ref b) => record.push_field(&b.to_string()),
             };
-            // dates are stored as numbers in Excel
-            // that's why we need the --dates-whitelist, so we can convert the number to a date.
+            // dates are stored as floats in Excel
+            // that's why we need the --dates-whitelist, so we can convert the float to a date.
             // However, with the XLSX format, we can get a cell's format as an attribute. So we can
             // automatically process a cell as a date, even if its column is NOT in the whitelist
             if float_flag {
