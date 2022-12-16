43c13ec299f3a837ccfaffc9c7b8e529d181d1f3
Joel Natividad <1980690+jqnatividad@users.noreply.github.com>
2022-12-15 21:26:24 -0500

    `safenames`: add tests for --reserved option

--- tests/test_safenames.rs
+++ tests/test_safenames.rs
@@ -292,3 +292,135 @@ fn safenames_invalid_mode() {
 
     wrk.assert_err(&mut cmd);
 }
+
+#[test]
+fn safenames_reserved_names_default() {
+    let wrk = Workdir::new("safenames");
+    wrk.create(
+        "in.csv",
+        vec![
+            svec![
+                "col1",
+                " This is a column with invalid chars!# and leading & trailing spaces ",
+                "_id",
+                "this is already a postgres safe column",
+                "1starts with 1",
+                "col1",
+                "col1"
+            ],
+            svec!["1", "b", "33", "1", "b", "33", "34"],
+            svec!["2", "c", "34", "3", "d", "31", "3"],
+        ],
+    );
+
+    let mut cmd = wrk.command("safenames");
+    cmd.arg("in.csv");
+
+    let got: Vec<Vec<String>> = wrk.read_stdout(&mut cmd);
+    let expected = vec![
+        svec![
+            "col1",
+            "this_is_a_column_with_invalid_chars___and_leading___trailing",
+            "_RESERVED__id",
+            "this_is_already_a_postgres_safe_column",
+            "_starts_with_1",
+            "col1_2",
+            "col1_3"
+        ],
+        svec!["1", "b", "33", "1", "b", "33", "34"],
+        svec!["2", "c", "34", "3", "d", "31", "3"],
+    ];
+    assert_eq!(got, expected);
+
+    let changed_headers = wrk.output_stderr(&mut cmd);
+    let expected_count = "6\n";
+    assert_eq!(changed_headers, expected_count);
+}
+
+#[test]
+fn safenames_reserved_names_specified() {
+    let wrk = Workdir::new("safenames");
+    wrk.create(
+        "in.csv",
+        vec![
+            svec![
+                "col1",
+                " This is a column with invalid chars!# and leading & trailing spaces ",
+                "waldo",
+                "this is already a postgres safe column",
+                "1starts with 1",
+                "col1",
+                "col1"
+            ],
+            svec!["1", "b", "33", "1", "b", "33", "34"],
+            svec!["2", "c", "34", "3", "d", "31", "3"],
+        ],
+    );
+
+    let mut cmd = wrk.command("safenames");
+    cmd.arg("--reserved").arg("waldo").arg("in.csv");
+
+    let got: Vec<Vec<String>> = wrk.read_stdout(&mut cmd);
+    let expected = vec![
+        svec![
+            "col1",
+            "this_is_a_column_with_invalid_chars___and_leading___trailing",
+            "_RESERVED_waldo",
+            "this_is_already_a_postgres_safe_column",
+            "_starts_with_1",
+            "col1_2",
+            "col1_3"
+        ],
+        svec!["1", "b", "33", "1", "b", "33", "34"],
+        svec!["2", "c", "34", "3", "d", "31", "3"],
+    ];
+    assert_eq!(got, expected);
+
+    let changed_headers = wrk.output_stderr(&mut cmd);
+    let expected_count = "6\n";
+    assert_eq!(changed_headers, expected_count);
+}
+
+#[test]
+fn safenames_reserved_names_specified_case_insensitive() {
+    let wrk = Workdir::new("safenames");
+    wrk.create(
+        "in.csv",
+        vec![
+            svec![
+                "col1",
+                " This is a column with invalid chars!# and leading & trailing spaces ",
+                "WaLdO",
+                "this is already a postgres safe column",
+                "1starts with 1",
+                "col1",
+                "col1"
+            ],
+            svec!["1", "b", "33", "1", "b", "33", "34"],
+            svec!["2", "c", "34", "3", "d", "31", "3"],
+        ],
+    );
+
+    let mut cmd = wrk.command("safenames");
+    cmd.arg("--reserved").arg("waldo").arg("in.csv");
+
+    let got: Vec<Vec<String>> = wrk.read_stdout(&mut cmd);
+    let expected = vec![
+        svec![
+            "col1",
+            "this_is_a_column_with_invalid_chars___and_leading___trailing",
+            "_RESERVED_waldo",
+            "this_is_already_a_postgres_safe_column",
+            "_starts_with_1",
+            "col1_2",
+            "col1_3"
+        ],
+        svec!["1", "b", "33", "1", "b", "33", "34"],
+        svec!["2", "c", "34", "3", "d", "31", "3"],
+    ];
+    assert_eq!(got, expected);
+
+    let changed_headers = wrk.output_stderr(&mut cmd);
+    let expected_count = "6\n";
+    assert_eq!(changed_headers, expected_count);
+}
