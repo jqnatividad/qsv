e39d7d5b789d51adb4a01b237e4bb0455c64d4e2
Joel Natividad <1980690+jqnatividad@users.noreply.github.com>
2023-01-01 18:56:50 -0500

    `index`: percentage sampling no longer requires an index

--- tests/test_sample.rs
+++ tests/test_sample.rs
@@ -53,10 +53,22 @@ fn sample_percentage_seed_no_index_error() {
     );
 
     let mut cmd = wrk.command("sample");
-    cmd.args(["--seed", "42"]).arg("0.4").arg("in.csv");
+    cmd.args(["--seed", "42"]).arg("0.6").arg("in.csv");
 
-    // error since percentage sampling requires an index
-    wrk.assert_err(&mut cmd);
+    // no error since percentage sampling no longer requires an index
+    // though note the results are different even with the same seed and
+    // sample size. This is because we use sample_reservoir method, not
+    // sample_random_access method
+    let got: Vec<Vec<String>> = wrk.read_stdout(&mut cmd);
+    let expected = vec![
+        svec!["R", "S"],
+        svec!["8", "h"],
+        svec!["2", "a"],
+        svec!["3", "d"],
+        svec!["7", "i"],
+        svec!["5", "f"],
+    ];
+    assert_eq!(got, expected);
 }
 
 #[test]
@@ -89,4 +101,29 @@ fn sample_percentage_seed_indexed() {
         svec!["6", "e"],
     ];
     assert_eq!(got, expected);
+}
+
+#[test]
+fn sample_percentage_negative_sample_size_error() {
+    let wrk = Workdir::new("sample_negative");
+    wrk.create_indexed(
+        "in.csv",
+        vec![
+            svec!["R", "S"],
+            svec!["1", "b"],
+            svec!["2", "a"],
+            svec!["3", "d"],
+            svec!["4", "c"],
+            svec!["5", "f"],
+            svec!["6", "e"],
+            svec!["7", "i"],
+            svec!["8", "h"],
+            svec!["8", "h"],
+        ],
+    );
+
+    let mut cmd = wrk.command("sample");
+    cmd.args(["--seed", "42"]).arg("-5").arg("in.csv");
+
+    wrk.assert_err(&mut cmd);
 }
