c97650821ba44c668947795e0ddfe9b04662f6d3
Joel Natividad <1980690+jqnatividad@users.noreply.github.com>
2023-01-08 19:25:02 -0500

    tweak `diff` test
    remove unneeded conditional compilation; correct workdir name; expanded sort comment

--- tests/test_diff.rs
+++ tests/test_diff.rs
@@ -1,9 +1,8 @@
-#![cfg(feature = "full")]
 use crate::workdir::Workdir;
 
 #[test]
 fn simple_diff() {
-    let wrk = Workdir::new("stats_rounding");
+    let wrk = Workdir::new("diff");
     let test_file = wrk.load_test_file("boston311-100.csv");
     let test_file2 = wrk.load_test_file("boston311-100-diff.csv");
 
@@ -14,7 +13,9 @@ fn simple_diff() {
 
     wrk.create("in2.csv", got);
 
-    // sort on case_enquiry_id
+    // sort on the 1st column, case_enquiry_id
+    // --select is set to 2 coz `diff` prepends
+    // a "diffresult" column
     let mut cmd = wrk.command("sort");
     cmd.arg("--select").arg("2").arg("in2.csv");
 
