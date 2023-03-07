ca0e182f5c5e767f9dee610c6c3c72ad9d7562ff
Joel Natividad <1980690+jqnatividad@users.noreply.github.com>
2023-03-07 07:02:26 -0500

    `luau`: use new string interpolation feature added in Luau 0.562
    instead of using concat to tediously compose return string

--- tests/test_luau.rs
+++ tests/test_luau.rs
@@ -272,8 +272,7 @@ END {
     -- note how we use the _ROWCOUNT special variable to get the number of rows
     min_amount = math.min(unpack(amount_array));
     max_amount = math.max(unpack(amount_array));
-    return ("Min/Max: " .. min_amount .. "/" .. max_amount ..
-       " Grand total of " .. _ROWCOUNT .. " rows: " .. grand_total);
+    return (`Min/Max: {min_amount}/{max_amount} Grand total of {_ROWCOUNT} rows: {grand_total}`);
 }!        
 "#,
     );
@@ -566,8 +565,7 @@ END {
     -- note how we use the _ROWCOUNT special variable to get the number of rows
     min_amount = math.min(unpack(amount_array));
     max_amount = math.max(unpack(amount_array));
-    return ("Min/Max: " .. min_amount .. "/" .. max_amount ..
-       " Grand total of " .. _ROWCOUNT .. " rows: " .. grand_total);
+    return (`Min/Max: {min_amount}/{max_amount} Grand total of {_ROWCOUNT} rows: {grand_total}`);
 }!
 "#,
     );
@@ -653,8 +651,7 @@ END {
     -- note how we use the _ROWCOUNT special variable to get the number of rows
     min_amount = math.min(unpack(amount_array));
     max_amount = math.max(unpack(amount_array));
-    return ("Min/Max: " .. min_amount .. "/" .. max_amount ..
-       " Grand total of " .. _ROWCOUNT .. " rows: " .. grand_total);
+    return (`Min/Max: {min_amount}/{max_amount} Grand total of {_ROWCOUNT} rows: {grand_total}`);
 }!
 "#,
     );
