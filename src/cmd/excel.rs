7ea090f79574fc5ee9973aae163083d9c572c18f
Joel Natividad <1980690+jqnatividad@users.noreply.github.com>
2023-03-14 18:36:58 -0400

    `excel`: clippy::match-same-arms
    warning: this match arm has an identical body to another arm
       --> src/cmd/excel.rs:509:21
        |
    509 |                     DataType::DateTime(ref f) => f.to_string(),
        |                     -------------------------^^^^^^^^^^^^^^^^^
        |                     |
        |                     help: try merging the arm patterns: `DataType::DateTime(ref f) | DataType::Float(ref f)`
        |
        = help: or try changing either arm body
    note: other arm here
       --> src/cmd/excel.rs:510:21
        |
    510 |                     DataType::Float(ref f) => f.to_string(),
        |                     ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^
        = help: for further information visit https://rust-lang.github.io/rust-clippy/master/index.html#match_same_arms
        = note: `-W clippy::match-same-arms` implied by `-W clippy::pedantic`

--- src/cmd/excel.rs
+++ src/cmd/excel.rs
@@ -506,8 +506,7 @@ pub fn run(argv: &[&str]) -> CliResult<()> {
                     DataType::Error(ref _e) => String::new(),
                     DataType::String(ref s) => s.to_string(),
                     DataType::Int(ref i) => i.to_string(),
-                    DataType::DateTime(ref f) => f.to_string(),
-                    DataType::Float(ref f) => f.to_string(),
+                    DataType::DateTime(ref f) | DataType::Float(ref f) => f.to_string(),
                     DataType::Bool(ref b) => b.to_string(),
                 };
                 record.push_field(&col_name);
