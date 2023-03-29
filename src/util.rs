9e8d9ceb77a0ee88c54d597e4eaaa5df4fbb0b2e
Joel Natividad <1980690+jqnatividad@users.noreply.github.com>
2023-03-28 20:18:43 -0400

    use variable shadowing for more straightforward var name
    [skip ci]

--- src/util.rs
+++ src/util.rs
@@ -83,10 +83,10 @@ pub fn njobs(flag_jobs: Option<usize>) -> usize {
     })
 }
 
-pub fn timeout_secs(timeout_option: u16) -> Result<u64, String> {
+pub fn timeout_secs(timeout: u16) -> Result<u64, String> {
     let timeout = match env::var("QSV_TIMEOUT") {
         Ok(val) => val.parse::<u16>().unwrap_or(30_u16),
-        Err(_) => timeout_option,
+        Err(_) => timeout,
     };
 
     if timeout > 3600 {
