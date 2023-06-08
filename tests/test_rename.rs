532de10c36b5321ad5406ff2b3d43fe2fad6e1a3
Joel Natividad <1980690+jqnatividad@users.noreply.github.com>
2023-06-08 07:34:13 -0400

    `rename`: add tests for _all_generic special value

--- tests/test_rename.rs
+++ tests/test_rename.rs
@@ -27,6 +27,32 @@ fn rename() {
 }
 
 #[test]
+fn rename_generic() {
+    let wrk = Workdir::new("rename");
+    wrk.create(
+        "in.csv",
+        vec![
+            svec!["R", "S"],
+            svec!["1", "b"],
+            svec!["2", "a"],
+            svec!["3", "d"],
+        ],
+    );
+
+    let mut cmd = wrk.command("rename");
+    cmd.arg("_all_generic").arg("in.csv");
+
+    let got: Vec<Vec<String>> = wrk.read_stdout(&mut cmd);
+    let expected = vec![
+        svec!["_col_1", "_col_2"],
+        svec!["1", "b"],
+        svec!["2", "a"],
+        svec!["3", "d"],
+    ];
+    assert_eq!(got, expected);
+}
+
+#[test]
 fn rename_noheaders() {
     let wrk = Workdir::new("rename_noheaders");
     wrk.create(
@@ -40,6 +66,27 @@ fn rename_noheaders() {
     let got: Vec<Vec<String>> = wrk.read_stdout(&mut cmd);
     let expected = vec![
         svec!["cola", "colb"],
+        svec!["1", "b"],
+        svec!["2", "a"],
+        svec!["3", "d"],
+    ];
+    assert_eq!(got, expected);
+}
+
+#[test]
+fn rename_noheaders_generic() {
+    let wrk = Workdir::new("rename_noheaders");
+    wrk.create(
+        "in.csv",
+        vec![svec!["1", "b"], svec!["2", "a"], svec!["3", "d"]],
+    );
+
+    let mut cmd = wrk.command("rename");
+    cmd.arg("_ALL_Generic").arg("--no-headers").arg("in.csv");
+
+    let got: Vec<Vec<String>> = wrk.read_stdout(&mut cmd);
+    let expected = vec![
+        svec!["_col_1", "_col_2"],
         svec!["1", "b"],
         svec!["2", "a"],
         svec!["3", "d"],
