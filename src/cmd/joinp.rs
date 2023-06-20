40825fdbbc9d51af1b526b8a0130f5f1201d8385
Joel Natividad <1980690+jqnatividad@users.noreply.github.com>
2023-06-20 08:11:17 -0400

    `joinp`: add asof join and --try_parsedates option

--- src/cmd/joinp.rs
+++ src/cmd/joinp.rs
@@ -45,8 +45,57 @@ joinp options:
                            equal to N * M, where N and M correspond to the
                            number of rows in the given data sets, respectively.
                            The columns1 and columns2 arguments are ignored.
+
+    --asof                 Do an 'asof' join. This is similar to a left outer
+                           join, except we match on nearest key rather than
+                           equal keys. Both CSV data sets must be sorted on
+                           the join columns. The output schema is the same as
+                           the first data set.
+    --strategy <arg>       The strategy to use for the asof join. The available are:
+                             backward - For each row in the first CSV data set,
+                                        we find the last row in the second data set
+                                        whose key is less than or equal to the key
+                                        in the first data set.
+                             forward -  For each row in the first CSV data set,
+                                        we find the first row in the second data set
+                                        whose key is greater than or equal to the key
+                                        in the first data set.
+                             nearest -  selects the last row in the second data set
+                                        whose value is nearest to the value in the
+                                        first data set.
+                           [default: backward]
+    --tolerance <arg>      The tolerance for the nearest asof join. This is only
+                           used when the nearest strategy is used. The
+                           tolerance is a positive integer that specifies
+                           the maximum number of rows to search for a match.
+
+                           If the join is done on a column of type Date, Time or
+                           DateTime, then the tolerance is interpreted using
+                           the following string language:
+                                1d - 1 day
+                                1h - 1 hour
+                                1m - 1 minute
+                                1s - 1 second
+                                1ms - 1 millisecond
+                                1us - 1 microsecond
+                                1ns - 1 nanosecond
+                                1w - 1 week
+                                1mo - 1 month
+                                1q - 1 quarter
+                                1y - 1 year
+                                1i - 1 index count
+                             Or combine them: “3d12h4m25s” # 3 days, 12 hours,
+                             4 minutes, and 25 seconds
+                             Suffix with “_saturating” to indicate that dates too
+                             large for their month should saturate at the largest date
+                             (e.g. 2022-02-29 -> 2022-02-28) instead of erroring.
+
     --nulls                When set, joins will work on empty fields.
                            Otherwise, empty fields are completely ignored.
+    --try-parsedates       When set, the join will attempt to parse the columns
+                           as dates. If the parse fails, the join will fail
+                           This is useful when the join columns are formatted as 
+                           dates in one CSV data set and strings in the other.
 
 Common options:
     -h, --help             Display this message
@@ -64,8 +113,10 @@ use std::{
 };
 
 use polars::{
+    chunked_array::object::{AsOfOptions, AsofStrategy},
+    datatypes::AnyValue,
     frame::hash_join::JoinType,
-    prelude::{CsvWriter, LazyCsvReader, LazyFileListReader, LazyFrame, SerWriter},
+    prelude::{CsvWriter, LazyCsvReader, LazyFileListReader, LazyFrame, SerWriter, SortOptions},
 };
 use serde::Deserialize;
 
@@ -73,19 +124,23 @@ use crate::{config::Delimiter, util, CliError, CliResult};
 
 #[derive(Deserialize)]
 struct Args {
-    arg_columns1:   String,
-    arg_input1:     String,
-    arg_columns2:   String,
-    arg_input2:     String,
-    flag_left:      bool,
-    flag_left_anti: bool,
-    flag_left_semi: bool,
-    flag_full:      bool,
-    flag_cross:     bool,
-    flag_output:    Option<String>,
-    flag_nulls:     bool,
-    flag_delimiter: Option<Delimiter>,
-    flag_quiet:     bool,
+    arg_columns1:        String,
+    arg_input1:          String,
+    arg_columns2:        String,
+    arg_input2:          String,
+    flag_left:           bool,
+    flag_left_anti:      bool,
+    flag_left_semi:      bool,
+    flag_full:           bool,
+    flag_cross:          bool,
+    flag_asof:           bool,
+    flag_strategy:       Option<String>,
+    flag_tolerance:      Option<String>,
+    flag_nulls:          bool,
+    flag_try_parsedates: bool,
+    flag_output:         Option<String>,
+    flag_delimiter:      Option<Delimiter>,
+    flag_quiet:          bool,
 }
 
 impl From<polars::error::PolarsError> for CliError {
@@ -95,21 +150,63 @@ impl From<polars::error::PolarsError> for CliError {
 }
 
 pub fn run(argv: &[&str]) -> CliResult<()> {
-    let args: Args = util::get_args(USAGE, argv)?;
-    let join = args.new_join()?;
+    let mut args: Args = util::get_args(USAGE, argv)?;
+
+    // always try to parse dates when its an asof join
+    // just in case the user doesn't specify it
+    // and they're using date/time/datetime columns
+    if args.flag_asof {
+        args.flag_try_parsedates = true;
+    }
+    let join = args.new_join(args.flag_try_parsedates)?;
+
     let join_shape = match (
         args.flag_left,
         args.flag_left_anti,
         args.flag_left_semi,
         args.flag_full,
         args.flag_cross,
+        args.flag_asof,
     ) {
-        (false, false, false, false, false) => join.polars_join(JoinType::Inner),
-        (true, false, false, false, false) => join.polars_join(JoinType::Left),
-        (false, true, false, false, false) => join.polars_join(JoinType::Anti),
-        (false, false, true, false, false) => join.polars_join(JoinType::Semi),
-        (false, false, false, true, false) => join.polars_join(JoinType::Outer),
-        (false, false, false, false, true) => join.polars_join(JoinType::Cross),
+        (false, false, false, false, false, false) => join.polars_join(JoinType::Inner, false),
+        (true, false, false, false, false, false) => join.polars_join(JoinType::Left, false),
+        (false, true, false, false, false, false) => join.polars_join(JoinType::Anti, false),
+        (false, false, true, false, false, false) => join.polars_join(JoinType::Semi, false),
+        (false, false, false, true, false, false) => join.polars_join(JoinType::Outer, false),
+        (false, false, false, false, true, false) => join.polars_join(JoinType::Cross, false),
+        (false, false, false, false, false, true) => {
+            // safety: flag_strategy is always is_some() as it has a default value
+            args.flag_strategy = Some(args.flag_strategy.unwrap().to_lowercase());
+            let asof_strategy = match args.flag_strategy.as_deref() {
+                Some("backward") | None => AsofStrategy::Backward,
+                Some("forward") => AsofStrategy::Forward,
+                Some("nearest") => AsofStrategy::Nearest,
+                Some(s) => return fail_clierror!("Invalid asof strategy: {}", s),
+            };
+            let mut asof_options = AsOfOptions::default();
+            asof_options.strategy = asof_strategy;
+
+            if asof_strategy == AsofStrategy::Nearest {
+                if let Some(ref tolerance) = args.flag_tolerance {
+                    // set try_parse_dates to true if the tolerance is set to a
+                    // non-numerical value, indicating that it is a
+                    // tolerance date string language
+                    let try_parse_dates = if let Some(tolerance) = &args.flag_tolerance {
+                        tolerance.parse::<i64>().is_err()
+                    } else {
+                        false
+                    };
+
+                    if try_parse_dates {
+                        asof_options.tolerance_str = Some(tolerance.into());
+                    } else {
+                        let numeric_tolerance = tolerance.parse::<i64>().unwrap();
+                        asof_options.tolerance = Some(AnyValue::Int64(numeric_tolerance));
+                    }
+                }
+            }
+            join.polars_join(JoinType::AsOf(asof_options), true)
+        }
         _ => fail!("Please pick exactly one join operation."),
     }?;
 
@@ -130,7 +227,7 @@ struct JoinStruct {
 }
 
 impl JoinStruct {
-    fn polars_join(self, jointype: JoinType) -> CliResult<(usize, usize)> {
+    fn polars_join(mut self, jointype: JoinType, asof_join: bool) -> CliResult<(usize, usize)> {
         let selcols1: Vec<_> = self.sel1.split(',').map(polars::lazy::dsl::col).collect();
         let selcols2: Vec<_> = self.sel2.split(',').map(polars::lazy::dsl::col).collect();
 
@@ -160,6 +257,20 @@ impl JoinStruct {
                 .join_builder()
                 .with(self.lf2.with_optimizations(optimize_all))
                 .how(JoinType::Cross)
+                .force_parallel(true)
+                .finish()
+                .collect()?
+        } else if asof_join {
+            // we need to sort by the asof column, as asof joins require sorted data
+            self.lf1 = self.lf1.sort(&self.sel1, SortOptions::default());
+            self.lf2 = self.lf2.sort(&self.sel2, SortOptions::default());
+
+            self.lf1
+                .with_optimizations(optimize_all)
+                .join_builder()
+                .with(self.lf2.with_optimizations(optimize_all))
+                .on(selcols1)
+                .how(jointype)
                 .force_parallel(true)
                 .finish()
                 .collect()?
@@ -198,7 +309,7 @@ impl JoinStruct {
 }
 
 impl Args {
-    fn new_join(&self) -> CliResult<JoinStruct> {
+    fn new_join(&self, try_parse_dates: bool) -> CliResult<JoinStruct> {
         let delim = if let Some(delimiter) = self.flag_delimiter {
             delimiter.as_byte()
         } else {
@@ -209,12 +320,14 @@ impl Args {
             .has_header(true)
             .with_missing_is_null(self.flag_nulls)
             .with_delimiter(delim)
+            .with_try_parse_dates(try_parse_dates)
             .finish()?;
 
         let lf2 = LazyCsvReader::new(&self.arg_input2)
             .has_header(true)
             .with_missing_is_null(self.flag_nulls)
             .with_delimiter(delim)
+            .with_try_parse_dates(try_parse_dates)
             .finish()?;
 
         Ok(JoinStruct {
