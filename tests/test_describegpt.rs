ecc217e0ac7c575a72181fe32815a4309d28173a
rzmk <mueez25@gmail.com>
2023-07-07 07:07:30 -0400

    describegpt: add valid and valid JSON tests

--- tests/test_describegpt.rs
+++ tests/test_describegpt.rs
@@ -1,10 +1,10 @@
-use crate::{qcheck, workdir::Workdir, CsvData};
+use crate::workdir::Workdir;
 
 // Providing an invalid API key with --openai-key without
 // the environment variable set should result in an error
 #[test]
-fn invalid_api_key() {
-    let wrk = Workdir::new("invalid_api_key");
+fn describegpt_invalid_api_key() {
+    let wrk = Workdir::new("describegpt");
     // Create a CSV file with sample data
     wrk.create_indexed(
         "in.csv",
@@ -27,4 +27,61 @@ fn invalid_api_key() {
     let got_stderr = wrk.output_stderr(&mut cmd);
     // Check that we receive the correct error message
     assert!(got_stderr.contains("Incorrect API key provided: INVALIDKEY"));
+}
+
+// Valid use of describegpt
+#[test]
+#[ignore = "Requires environment variable to be set."]
+fn describegpt_valid() {
+    let wrk = Workdir::new("describegpt");
+
+    // Create a CSV file with sample data
+    wrk.create_indexed(
+        "in.csv",
+        vec![
+            svec!["letter", "number"],
+            svec!["alpha", "13"],
+            svec!["beta", "24"],
+            svec!["gamma", "37"],
+        ],
+    );
+
+    // Run the command
+    let mut cmd = wrk.command("describegpt");
+    cmd.arg("--all").arg("in.csv");
+
+    // Check that the command ran successfully
+    wrk.assert_success(&mut cmd);
+}
+
+// Valid use of describegpt with --json
+#[test]
+#[ignore = "Requires environment variable to be set."]
+fn describegpt_valid_json() {
+    let wrk = Workdir::new("describegpt");
+
+    // Create a CSV file with sample data
+    wrk.create_indexed(
+        "in.csv",
+        vec![
+            svec!["letter", "number"],
+            svec!["alpha", "13"],
+            svec!["beta", "24"],
+            svec!["gamma", "37"],
+        ],
+    );
+
+    // Run the command
+    let mut cmd = wrk.command("describegpt");
+    cmd.arg("--all").arg("--json").arg("in.csv");
+
+    // Check that the output is valid JSON
+    let got = wrk.stdout::<String>(&mut cmd);
+    match serde_json::from_str::<serde_json::Value>(&got) {
+        Ok(_) => (),
+        Err(e) => assert!(false, "Error parsing JSON: {e}"),
+    }
+
+    // Check that the command ran successfully
+    wrk.assert_success(&mut cmd);
 }
