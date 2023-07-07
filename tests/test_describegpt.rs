7100ac112db1a917323263b4d5d643d400c3ff95
rzmk <mueez25@gmail.com>
2023-07-07 05:42:11 -0400

    describegpt: invalid_api_key test

--- tests/test_describegpt.rs
+++ tests/test_describegpt.rs
@@ -1,8 +1,11 @@
 use crate::{qcheck, workdir::Workdir, CsvData};
 
+// Providing an invalid API key with --openai-key without
+// the environment variable set should result in an error
 #[test]
-fn count_simple() {
-    let wrk = Workdir::new("count_simple");
+fn invalid_api_key() {
+    let wrk = Workdir::new("invalid_api_key");
+    // Create a CSV file with sample data
     wrk.create_indexed(
         "in.csv",
         vec![
@@ -12,172 +15,16 @@ fn count_simple() {
             svec!["gamma", "37"],
         ],
     );
-    let mut cmd = wrk.command("count");
-    cmd.arg("in.csv");
 
-    let got: String = wrk.stdout(&mut cmd);
-    let expected = "3";
-    assert_eq!(got, expected.to_string());
-}
+    // Run the command
+    let mut cmd = wrk.command("describegpt");
+    cmd.arg("--all")
+        .arg("--json")
+        .args(["--openai-key", "INVALIDKEY"])
+        .arg("in.csv");
 
-#[test]
-fn count_width() {
-    let wrk = Workdir::new("count_width");
-    wrk.create_indexed(
-        "in.csv",
-        vec![
-            svec!["letter", "number"],
-            svec!["alpha", "13"],
-            svec!["beta", "24"],
-        ],
-    );
-    let mut cmd = wrk.command("count");
-    cmd.arg("--width").arg("in.csv");
-
-    let got: String = wrk.stdout(&mut cmd);
-    let expected = "2;9";
-    assert_eq!(got, expected.to_string());
-}
-
-#[test]
-fn count_comments() {
-    let wrk = Workdir::new("count_comments");
-
-    wrk.create(
-        "in.csv",
-        vec![
-            svec!["# this is a comment", ""],
-            svec!["# next comment", ""],
-            svec!["letter", "number"],
-            svec!["alpha", "13"],
-            svec!["beta", "24"],
-            svec!["# comment here too!", "24"],
-        ],
-    );
-    let mut cmd = wrk.command("count");
-    cmd.arg("in.csv").env("QSV_COMMENT_CHAR", "#");
-
-    let got: String = wrk.stdout(&mut cmd);
-    let expected = "2";
-    assert_eq!(got, expected.to_string());
-}
-
-/// This tests whether `qsv count` gets the right answer.
-///
-/// It does some simple case analysis to handle whether we want to test counts
-/// in the presence of headers and/or indexes.
-fn prop_count_len(
-    name: &str,
-    rows: CsvData,
-    headers: bool,
-    idx: bool,
-    noheaders_env: bool,
-    human_readable: bool,
-) -> bool {
-    let mut expected_count = rows.len();
-    if headers && expected_count > 0 {
-        expected_count -= 1;
-    }
-
-    let wrk = Workdir::new(name);
-    if idx {
-        wrk.create_indexed("in.csv", rows);
-    } else {
-        wrk.create("in.csv", rows);
-    }
-
-    let mut cmd = wrk.command("count");
-    if !headers {
-        cmd.arg("--no-headers");
-    }
-    if noheaders_env {
-        cmd.env("QSV_NO_HEADERS", "1");
-    }
-    if human_readable {
-        cmd.arg("--human-readable");
-    }
-    cmd.arg("in.csv");
-
-    if human_readable {
-        use thousands::Separable;
-
-        let got_count: String = wrk.stdout(&mut cmd);
-        let expected_count_commas = expected_count.separate_with_commas();
-
-        rassert_eq!(got_count, expected_count_commas)
-    } else {
-        let got_count: usize = wrk.stdout(&mut cmd);
-        rassert_eq!(got_count, expected_count)
-    }
-}
-
-#[test]
-fn prop_count() {
-    fn p(rows: CsvData) -> bool {
-        prop_count_len("prop_count", rows, false, false, false, false)
-    }
-    qcheck(p as fn(CsvData) -> bool);
-}
-
-#[test]
-fn prop_count_human_readable() {
-    fn p(rows: CsvData) -> bool {
-        prop_count_len("prop_count", rows, false, false, false, true)
-    }
-    qcheck(p as fn(CsvData) -> bool);
-}
-
-#[test]
-fn prop_count_headers() {
-    fn p(rows: CsvData) -> bool {
-        prop_count_len("prop_count_headers", rows, true, false, false, false)
-    }
-    qcheck(p as fn(CsvData) -> bool);
-}
-
-#[test]
-fn prop_count_headers_human_readable() {
-    fn p(rows: CsvData) -> bool {
-        prop_count_len("prop_count_headers", rows, true, false, false, true)
-    }
-    qcheck(p as fn(CsvData) -> bool);
-}
-
-#[test]
-fn prop_count_indexed() {
-    fn p(rows: CsvData) -> bool {
-        prop_count_len("prop_count_indexed", rows, false, true, false, false)
-    }
-    qcheck(p as fn(CsvData) -> bool);
-}
-
-#[test]
-fn prop_count_indexed_headers() {
-    fn p(rows: CsvData) -> bool {
-        prop_count_len("prop_count_indexed_headers", rows, true, true, false, false)
-    }
-    qcheck(p as fn(CsvData) -> bool);
-}
-
-#[test]
-fn prop_count_noheaders_env() {
-    fn p(rows: CsvData) -> bool {
-        prop_count_len("prop_count_noheaders_env", rows, false, false, true, false)
-    }
-    qcheck(p as fn(CsvData) -> bool);
-}
-
-#[test]
-fn prop_count_noheaders_indexed_env() {
-    fn p(rows: CsvData) -> bool {
-        prop_count_len(
-            "prop_count_noheaders_indexed_env",
-            rows,
-            false,
-            true,
-            true,
-            false,
-        )
-    }
-    qcheck(p as fn(CsvData) -> bool);
+    // Error message
+    let got_stderr = wrk.output_stderr(&mut cmd);
+    // Check that we receive the correct error message
+    assert!(got_stderr.contains("Incorrect API key provided: INVALIDKEY"));
 }
