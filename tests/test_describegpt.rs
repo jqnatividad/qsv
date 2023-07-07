2ccce0789f961651a40073a350b825ba38d17335
rzmk <mueez25@gmail.com>
2023-07-07 12:32:29 -0400

    describegpt: --user-agent test

--- tests/test_describegpt.rs
+++ tests/test_describegpt.rs
@@ -3,6 +3,7 @@ use crate::workdir::Workdir;
 // Providing an invalid API key with --openai-key without
 // the environment variable set should result in an error
 #[test]
+// #[ignore = "Requires environment variable to NOT be set."]
 fn describegpt_invalid_api_key() {
     let wrk = Workdir::new("describegpt");
     // Create a CSV file with sample data
@@ -28,6 +29,34 @@ fn describegpt_invalid_api_key() {
     let got_stderr = wrk.output_stderr(&mut cmd);
     // Check that we receive the correct error message
     assert!(got_stderr.contains("Incorrect API key provided: INVALIDKEY"));
+}
+
+// Verify --user-agent is passed to OpenAI
+#[test]
+#[ignore = "Requires environment variable to be set."]
+fn describegpt_user_agent() {
+    let wrk = Workdir::new("describegpt");
+    // Create a CSV file with sample data
+    wrk.create_indexed(
+        "in.csv",
+        vec![
+            svec!["letter", "number"],
+            svec!["alpha", "13"],
+            svec!["beta", "24"],
+            svec!["gamma", "37"],
+        ],
+    );
+
+    // Run the command
+    let mut cmd = wrk.command("describegpt");
+    cmd.arg("in.csv")
+        .arg("--all")
+        .arg("--json")
+        .args(["--user-agent", "Mozilla/5.0 (platform; rv:geckoversion) Gecko/geckotrail Firefox/firefoxversion"])
+        .args(["--max-tokens", "1000"]);
+
+    // Check that the command ran successfully
+    wrk.assert_success(&mut cmd);
 }
 
 // Valid use of describegpt
