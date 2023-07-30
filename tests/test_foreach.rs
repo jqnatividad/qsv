0c20486471863c73b0bcd1c967120e4ff89550b6
Joel Natividad <1980690+jqnatividad@users.noreply.github.com>
2023-07-30 10:59:34 -0400

    `foreach`: add tests
    - for issue 1171
    - for multiple {} pairs in template pattern

--- tests/test_foreach.rs
+++ tests/test_foreach.rs
@@ -17,6 +17,54 @@ fn foreach() {
 }
 
 #[test]
+fn foreach_multiple_braces() {
+    let wrk = Workdir::new("foreach");
+    wrk.create(
+        "data.csv",
+        vec![svec!["name"], svec!["John"], svec!["Mary"]],
+    );
+    let mut cmd = wrk.command("foreach");
+    cmd.arg("name")
+        .arg("echo 'NAME = {}, {}, {}'")
+        .arg("data.csv");
+
+    let got: Vec<Vec<String>> = wrk.read_stdout(&mut cmd);
+    let expected = vec![
+        svec!["NAME = John", " John", " John"],
+        svec!["NAME = Mary", " Mary", " Mary"],
+    ];
+    assert_eq!(got, expected);
+}
+
+#[test]
+fn foreach_special_chars_1171() {
+    let wrk = Workdir::new("foreach_special_chars");
+    wrk.create(
+        "data.csv",
+        vec![
+            svec!["host"],
+            svec!["omadhina.co.NA"],
+            svec!["https://www.google.com"],
+            svec!["www.apple.com"],
+            svec!["https://civic-data-ecosystem.github.io"],
+        ],
+    );
+    let mut cmd = wrk.command("foreach");
+    cmd.arg("host")
+        .arg("echo 'dig +short {} a'")
+        .arg("data.csv");
+
+    let got: Vec<Vec<String>> = wrk.read_stdout(&mut cmd);
+    let expected = vec![
+        svec!["dig +short omadhina.co.NA a"],
+        svec!["dig +short https://www.google.com a"],
+        svec!["dig +short www.apple.com a"],
+        svec!["dig +short https://civic-data-ecosystem.github.io a"],
+    ];
+    assert_eq!(got, expected);
+}
+
+#[test]
 fn foreach_unify() {
     let wrk = Workdir::new("foreach_unify");
     wrk.create(
