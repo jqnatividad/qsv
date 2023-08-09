563119fe2af0f30664a95b2b91711c3c025d6bb2
Joel Natividad <1980690+jqnatividad@users.noreply.github.com>
2023-08-09 07:55:27 -0400

    add comments as to the purpose of the different build envvars
    [skip ci]

--- build.rs
+++ build.rs
@@ -1,8 +1,11 @@
 fn main() {
+    // we use TARGET in --version and user-agent strings
     println!(
         "cargo:rustc-env=TARGET={}",
         std::env::var("TARGET").unwrap()
     );
+    // QSV_KIND is used to determine how qsv was built and is displayed in --version
+    // check PERFORMANCE.md for more info
     println!(
         "cargo:rustc-env=QSV_KIND={}",
         std::env::var("QSV_KIND").unwrap_or_else(|_| "compiled".to_string())
