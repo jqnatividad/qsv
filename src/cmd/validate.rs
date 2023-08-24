badabffafe87095b3e0a01edf7714422ccee8dda
Joel Natividad <1980690+jqnatividad@users.noreply.github.com>
2023-08-24 15:28:17 -0400

    `validate`: expanded usage text re `schema`-`validate` workflow
    [skip ci]

--- src/cmd/validate.rs
+++ src/cmd/validate.rs
@@ -6,12 +6,26 @@ This mode is invoked if a JSON Schema file is provided. The CSV data is validate
 Schema, and invalid records are put into an "invalid" file, with the rest of the records put into
 a "valid"" file.
 
-It uses the JSON Schema Validation draft 2020-12 specification. See
-https://json-schema.org/draft/2020-12/json-schema-validation.html
+When errors are found, a "validation-errors.tsv" file is created with the following columns:
+
+* row_number: the row number of the invalid record
+* field: the field name of the invalid field
+* error: the error message
+
+It uses the JSON Schema Validation draft 2020-12 specification to validate the file. It validates
+not only the structure of the file, but the data types and domain/range of the fields as well.
+See https://json-schema.org/draft/2020-12/json-schema-validation.html
 
 You can create a JSON Schema file from a reference CSV file using the `qsv schema` command.
 Once the schema is created, you can fine-tune it to your needs and use it to validate other CSV
 files that have the same structure.
+
+Be sure to select a “training” CSV file that is representative of the data you want to validate
+when creating a schema. The data types, domain/range and regular expressions inferred from the
+reference CSV file should be appropriate for the data you want to validate. 
+
+Typically, after creating a schema, you should edit it to fine-tune each field's inferred
+validation rules.
 
 Example output files from `mydata.csv`. If piped from stdin, then filename is `stdin.csv`.
 
@@ -19,7 +33,7 @@ Example output files from `mydata.csv`. If piped from stdin, then filename is `s
 * mydata.csv.invalid
 * mydata.csv.validation-errors.tsv
 
-The JSON Schema can be a local file or a URL.
+The JSON Schema file can be a local file or a URL.
 
 RFC 4180 VALIDATION MODE:
 If run without a JSON Schema file, the CSV is validated if it complies with the RFC 4180 CSV 
