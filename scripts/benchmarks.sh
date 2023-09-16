8ca5cb7fc680f8489b8c1ecfd658762ea947dcac
Joel Natividad <1980690+jqnatividad@users.noreply.github.com>
2023-09-16 00:34:52 -0400

    `benchmarks`: refactor
    - added benchmark name pattern filtering
    - moved configurable vars upfront for benchmark customization
    - made number of warmups and benchmark runs configurable
    - added more qsv commands for benchmarking
    - ensured sqlp commands were properly escaped
    - fixed luau command
    
    [skip ci]

--- scripts/benchmarks.sh
+++ scripts/benchmarks.sh
@@ -1,5 +1,10 @@
 #!/bin/bash
-
+#
+# Usage: ./benchmarks.sh <pattern>
+#  where <pattern> is a substring of the command name to be benchmarked.
+#  e.g. ./benchmarks.sh sort - will run benchmarks with "sort" in the benchmark name
+#  if <pattern> is omitted, all commands will be benchmarked.
+#
 # This script benchmarks Quicksilver (qsv) using a 520mb, 41 column, 1M row sample of
 # NYC's 311 data. If it doesn't exist on your system, it will be downloaded for you.
 #
@@ -27,9 +32,17 @@ set -e
 
 pat="$1"
 echo "Setting up benchmarking environment..."
-
 SECONDS=0
+
+# configurable variables - change as needed to reflect your environment/workloads
 qsv_bin=qsv
+benchmark_data_url=https://raw.githubusercontent.com/wiki/jqnatividad/qsv/files/NYC_311_SR_2010-2020-sample-1M.7z
+# where to download the benchmark data compressed file - this could be a zip or 7z file
+datazip=/tmp/NYC_311_SR_2010-2020-sample-1M.7z
+# where to store the benchmark data
+data=NYC_311_SR_2010-2020-sample-1M.csv
+warmup_runs=2
+benchmark_runs=3
 
 # check if qsv is installed
 if ! command -v "$qsv_bin" &> /dev/null
@@ -62,14 +75,14 @@ then
     exit
 fi
 
-datazip=/tmp/NYC_311_SR_2010-2020-sample-1M.7z
-data=NYC_311_SR_2010-2020-sample-1M.csv
-
 if [ ! -r "$data" ]; then
   echo "Downloading benchmarking data..."
-  curl -sS https://raw.githubusercontent.com/wiki/jqnatividad/qsv/files/NYC_311_SR_2010-2020-sample-1M.7z > "$datazip"
+  curl -sS "$benchmark_data_url" > "$datazip"
   "$sevenz_bin" e -y "$datazip"
 fi
+
+rowcount=$("$qsv_bin" count "$data")
+printf "Benchmark data rowcount: %'.0f\n" $rowcount
 
 if [ ! -r communityboards.csv ]; then
   echo "Downloading community board data..."
@@ -118,28 +131,38 @@ function run {
   local name="$1"
   shift
 
-  if [ -z "$index" ]; then
-    commands_without_index_name+=("$name")
-    add_command "without_index" "$@"
-  else
-    commands_with_index_name+=("$name")
-    add_command "with_index" "$@"
+  if [[ "$name" == *"$pat"* ]]; then
+    if [ -z "$index" ]; then
+      commands_without_index_name+=("$name")
+      add_command "without_index" "$@"
+    else
+      commands_with_index_name+=("$name")
+      add_command "with_index" "$@"
+    fi
   fi
 }
 
 # ---------------------------------------
 # Queue commands for benchmarking
 # commands with an --index prefix will be benchmarked with an index
+# template: run <benchmark name> <qsv command> <qsv command args>
 
-echo "Queueing commands for benchmarking..."
-run apply_op_string "$qsv_bin apply operations lower Agency  $data"
-run apply_op_similarity "$qsv_bin apply operations lower,simdln Agency --comparand brooklyn --new-column Agency_sim-brooklyn_score  $data"
-run apply_op_eudex "$qsv_bin apply operations lower,eudex Agency --comparand Queens --new-column Agency_queens_soundex  $data" 
-run apply_datefmt "$qsv_bin apply datefmt \"Created Date\"  $data"
+run apply_calcconv "$qsv_bin apply calcconv --formatstr \"{Unique Key} meters in miles\" --new-column new_col $data"
+run apply_datefmt "$qsv_bin apply datefmt \"Created Date\" $data"
+run apply_datefmt_multi "$qsv_bin apply datefmt \"Created Date,Closed Date,Due Date\" $data"
+run apply_dynfmt "$qsv_bin apply dynfmt \"{Created Date} {Complaint Type} - {BBL} {City}\" $data"
 run apply_emptyreplace "$qsv_bin" apply emptyreplace \"Bridge Highway Name\" --replacement Unspecified "$data"
+run apply_op_eudex "$qsv_bin apply operations lower,eudex Agency --comparand Queens --new-column Agency_queens_soundex $data" 
+run apply_op_string "$qsv_bin apply operations lower Agency $data"
+run apply_op_similarity "$qsv_bin apply operations lower,simdln Agency --comparand brooklyn --new-column Agency_sim-brooklyn_score $data"
+run behead "$qsv_bin" behead "$data"
+run cat_columns "$qsv_bin" cat columns "$data" data_unsorted.csv
+run cat_rows "$qsv_bin" cat rows "$data" data_unsorted.csv
+run cat_rowskey "$qsv_bin" cat rowskey "$data" data_unsorted.csv
 run count "$qsv_bin" count "$data"
 run --index count_index "$qsv_bin" count "$data"
 run dedup "$qsv_bin" dedup "$data"
+run dedup_sorted "$qsv_bin" dedup data_sorted.csv
 run diff "$qsv_bin" diff "$data" data_unsorted.csv
 run enum "$qsv_bin" enum "$data"
 run exclude "$qsv_bin" exclude \'Incident Zip\' "$data" \'Incident Zip\' data_to_exclude.csv
@@ -168,6 +191,7 @@ run pseudo "$qsv_bin" pseudo \'Unique Key\' "$data"
 run rename "$qsv_bin" rename \'unique_key,created_date,closed_date,agency,agency_name,complaint_type,descriptor,loctype,zip,addr1,street,xstreet1,xstreet2,inter1,inter2,addrtype,city,landmark,facility_type,status,due_date,res_desc,res_act_date,comm_board,bbl,boro,xcoord,ycoord,opendata_type,parkname,parkboro,vehtype,taxi_boro,taxi_loc,bridge_hwy_name,bridge_hwy_dir,ramp,bridge_hwy_seg,lat,long,loc\' "$data"
 run replace "$qsv_bin" replace \'zip\' \'postal\' "$data"
 run reverse "$qsv_bin" reverse "$data"
+run safenames "$qsv_bin" safenames "$data"
 run sample_10 "$qsv_bin" sample 10 "$data"
 run --index sample_10_index "$qsv_bin" sample 10 "$data"
 run sample_1000 "$qsv_bin" sample 1000 "$data"
@@ -181,6 +205,7 @@ run --index sample_100000_seeded_index_faster "$qsv_bin" sample --faster --seed 
 run --index sample_25pct_index "$qsv_bin" sample 0.25 "$data"
 run --index sample_25pct_seeded_index "$qsv_bin" sample 0.25 --seed 42 "$data"
 run schema "$qsv_bin" schema "$data"
+run --index schema_index "$qsv_bin" schema "$data"
 run search "$qsv_bin" search -s \'Agency Name\' "'(?i)us'" "$data"
 run search_unicode "$qsv_bin" search --unicode -s \'Agency Name\' "'(?i)us'" "$data"
 run searchset "$qsv_bin" searchset searchset_patterns.txt "$data"
@@ -190,6 +215,9 @@ run select "$qsv_bin" select \'Agency,Community Board\' "$data"
 run select_regex "$qsv_bin" select /^L/ "$data"
 run slice_one_middle "$qsv_bin" slice -i 500000 "$data"
 run --index slice_one_middle_index "$qsv_bin" slice -i 500000 "$data"
+run snappy_compress "$qsv_bin" snappy compress "$data" --output benchmark_work.snappy
+run snappy_decompress "$qsv_bin" snappy decompress benchmark_work.snappy
+run snappy_validate "$qsv_bin" snappy validate benchmark_work.snappy
 run sort "$qsv_bin" sort -s \'Incident Zip\' "$data"
 run sort_random_seeded "$qsv_bin" sort --random --seed 42 "$data"
 run sortcheck_sorted "$qsv_bin" sortcheck data_sorted.csv
@@ -198,7 +226,14 @@ run sortcheck_unsorted_all "$qsv_bin" sortcheck --all data_unsorted.csv
 run split "$qsv_bin" split --size 50000 split_tempdir "$data"
 run --index split_index "$qsv_bin" split --size 50000 split_tempdir "$data"
 run --index split_index_j1 "$qsv_bin" split --size 50000 -j 1 split_tempdir "$data"
-run sqlp "$qsv_bin" sqlp  "$data" "'select * from _t_1 where \"Complaint Type\"=\'Noise\' and Borough=\'BROOKLYN\' limit 10'"
+run sqlp "$qsv_bin" sqlp  "$data" -Q '"select * from _t_1 where \"Complaint Type\"='\''Noise'\'' and Borough='\''BROOKLYN'\''"'
+run sqlp_format_arrow "$qsv_bin" sqlp --format arrow "$data" -Q '"select * from _t_1 where \"Complaint Type\"='\''Noise'\'' and Borough='\''BROOKLYN'\''"'
+run sqlp_format_json "$qsv_bin" sqlp --format json "$data" -Q '"select * from _t_1 where \"Complaint Type\"='\''Noise'\'' and Borough='\''BROOKLYN'\''"'
+run sqlp_format_parquet "$qsv_bin" sqlp --format parquet "$data" -Q '"select * from _t_1 where \"Complaint Type\"='\''Noise'\'' and Borough='\''BROOKLYN'\''"'
+run sqlp_format_parquet_statistics "$qsv_bin" sqlp --format parquet --statistics "$data" -Q '"select * from _t_1 where \"Complaint Type\"='\''Noise'\'' and Borough='\''BROOKLYN'\''"'
+run sqlp_lowmemory "$qsv_bin" sqlp  "$data" -Q --low-memory '"select * from _t_1 where \"Complaint Type\"='\''Noise'\'' and Borough='\''BROOKLYN'\''"'
+run sqlp_nooptimizations "$qsv_bin" sqlp  "$data" -Q --no-optimizations '"select * from _t_1 where \"Complaint Type\"='\''Noise'\'' and Borough='\''BROOKLYN'\''"'
+run sqlp_tryparsedates "$qsv_bin" sqlp  "$data" -Q --try-parsedates '"select * from _t_1 where \"Complaint Type\"='\''Noise'\'' and Borough='\''BROOKLYN'\''"'
 run stats "$qsv_bin" stats --force "$data"
 run --index stats_index "$qsv_bin" stats --force "$data"
 run --index stats_index_j1 "$qsv_bin" stats -j 1 --force "$data"
@@ -209,8 +244,25 @@ run --index stats_everything_index "$qsv_bin" stats "$data" --force --everything
 run --index stats_everything_infer_dates_index "$qsv_bin" stats "$data" --force --everything --infer-dates
 run --index stats_everything_index_j1 "$qsv_bin" stats "$data" --force --everything -j 1
 run table "$qsv_bin" table "$data"
+run to_xlsx "$qsv_bin" to xlsx benchmark_data.xlsx "$data"
+run to_sqlite "$qsv_bin" to sqlite benchmark_work.db "$data"
+run to_parquet "$qsv_bin" to parquet benchmark_work "$data"
+run to_datapackage "$qsv_bin" to datapackage benchmark_work.json "$data"
+run excel "$qsv_bin" excel benchmark_data.xlsx
+run to_jsonl "$qsv_bin" to jsonl "$data"
+run --index to_jsonl_index "$qsv_bin" to jsonl "$data"
 run transpose "$qsv_bin" transpose "$data"
+run transpose_multipass "$qsv_bin" transpose --multipass "$data"
 run validate "$qsv_bin" validate "$data" "$schema"
+run validate_no_schema "$qsv_bin" validate "$data"
+run --index validate_index "$qsv_bin" validate "$data" "$schema"
+run --index validate_no_schema_index "$qsv_bin" validate "$data"
+
+# show count of commands to be benchmarked
+with_index_count=${#commands_with_index[@]}
+wo_index_count=${#commands_without_index[@]}
+total_count=$((with_index_count + wo_index_count))
+printf "Total commands to be benchmarked: $total_count, w/o index: $wo_index_count, with index: $with_index_count\n\n"
 
 # ---------------------------------------
 # Prepare benchmark results directory
@@ -244,10 +296,12 @@ now=$(date +"%Y-%m-%d-%H")
 # each command is run five times. Two warm-up runs & three benchmarked runs.
 echo "Benchmarking WITHOUT INDEX..."
 idx=0
+name_idx=1
 for command_no_index in "${commands_without_index[@]}"; do
   rm -f "$data".idx
-  echo "${commands_without_index_name[$idx]}"
-  hyperfine --warmup 2 -i --runs 3 --export-csv results/hf_result.csv "$command_no_index"
+  echo "$name_idx. ${commands_without_index_name[$idx]}"
+  hyperfine --warmup "$warmup_runs" -i --runs "$benchmark_runs" --export-csv results/hf_result.csv \
+    "$command_no_index"
   
   # prepend version, tstamp & benchmark name to the hyperfine results
   echo "version,tstamp,name" > results/results_work.csv
@@ -265,6 +319,7 @@ for command_no_index in "${commands_without_index[@]}"; do
     -o results/results_work.csv
   mv results/results_work.csv results/latest_results.csv
   ((idx++))
+  ((name_idx++))
 done
 
 # ---------------------------------------
@@ -276,8 +331,9 @@ rm -f "$data".idx
 
 idx=0
 for command_with_index in "${commands_with_index[@]}"; do
-  echo "${commands_with_index_name[$idx]}"
-  hyperfine --warmup 2 -i --runs 3 --export-csv results/hf_result.csv "$command_with_index"
+  echo "$name_idx. ${commands_with_index_name[$idx]}"
+  hyperfine --warmup "$warmup_runs" -i --runs "$benchmark_runs" --export-csv results/hf_result.csv \
+    "$command_with_index"
   echo "version,tstamp,name" > results/results_work.csv
   echo "$version,$now,${commands_with_index_name[$idx]}" >> results/results_work.csv
   "$qsv_bin" select '!command' results/hf_result.csv -o results/hf_result_nocmd.csv
@@ -287,6 +343,7 @@ for command_with_index in "${commands_with_index[@]}"; do
     -o results/results_work.csv
   mv results/results_work.csv results/latest_results.csv
   ((idx++))
+  ((name_idx++))
 done
 
 # ---------------------------------------
@@ -304,9 +361,8 @@ done
 # compute records per second for each benchmark using qsv, rounding to 3 decimal places
 # we get the rowcount, just in case the benchmark data was modified by the user to tailor
 # the benchmark to their system/workload.
-rowcount=$("$qsv_bin" count "$data")
-"$qsv_bin" luau map recs_per_sec \
-   "'recs_per_sec=("$rowcount" / mean); return tonumber(string.format(\"%.3f\",recs_per_sec))'" \
+luau_cmd="recs_per_sec=( $rowcount / mean); return tonumber(string.format(\"%.3f\",recs_per_sec))"
+"$qsv_bin" luau map recs_per_sec "$luau_cmd" \
    results/results_work.csv -o results/latest_results.csv
 
 # Concatenate the final results of this run to results/bechmark_results.csv
@@ -320,5 +376,7 @@ rm -f results/hf_result_nocmd.csv
 rm -f results/results_work.csv
 rm -f results/entry.csv
 rm -r -f split_tempdir
+rm -f benchmark_work.*
+rm -r -f benchmark_work
 
-echo "Benchmark results completed. Elapsed time: $SECONDS seconds."
+echo "$total_count benchmarks executed. Elapsed time: $SECONDS seconds."
