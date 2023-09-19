6b47436a4ffa12c5ab838a62449cbf03372f8f72
Joel Natividad <1980690+jqnatividad@users.noreply.github.com>
2023-09-19 06:02:31 -0400

    rust fmt

--- src/cmd/geocode.rs
+++ src/cmd/geocode.rs
@@ -371,8 +371,11 @@ struct Admin1Filter {
 }
 
 static QSV_VERSION: &str = env!("CARGO_PKG_VERSION");
-static DEFAULT_GEOCODE_INDEX_FILENAME: &str =
-    concat!("qsv-", env!("CARGO_PKG_VERSION"), "-geocode-index.bincode.new");
+static DEFAULT_GEOCODE_INDEX_FILENAME: &str = concat!(
+    "qsv-",
+    env!("CARGO_PKG_VERSION"),
+    "-geocode-index.bincode.new"
+);
 
 static DEFAULT_CITIES_NAMES_URL: &str =
     "https://download.geonames.org/export/dump/alternateNamesV2.zip";
