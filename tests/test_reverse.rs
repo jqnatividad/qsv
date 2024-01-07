3fea1d896b7c7e88896565c9e33231b37b901a72
Joel Natividad <1980690+jqnatividad@users.noreply.github.com>
2024-01-07 09:01:25 -0500

    `tests`: add property tests for reverse indexed mode

--- tests/test_reverse.rs
+++ tests/test_reverse.rs
@@ -39,3 +39,43 @@ fn prop_reverse_no_headers() {
     }
     qcheck(p as fn(CsvData) -> bool);
 }
+
+fn prop_reverse_indexed(name: &str, rows: CsvData, headers: bool) -> bool {
+    let wrk = Workdir::new(name);
+    wrk.create_indexed("in.csv", rows.clone());
+
+    let mut cmd = wrk.command("reverse");
+    cmd.arg("in.csv");
+    if !headers {
+        cmd.arg("--no-headers");
+    }
+
+    let got: Vec<Vec<String>> = wrk.read_stdout(&mut cmd);
+    let mut expected = rows.to_vecs();
+    let headers = if headers && !expected.is_empty() {
+        expected.remove(0)
+    } else {
+        vec![]
+    };
+    expected.reverse();
+    if !headers.is_empty() {
+        expected.insert(0, headers);
+    }
+    rassert_eq!(got, expected)
+}
+
+#[test]
+fn prop_reverse_headers_indexed() {
+    fn p(rows: CsvData) -> bool {
+        prop_reverse_indexed("prop_reverse_headers_indexed", rows, true)
+    }
+    qcheck(p as fn(CsvData) -> bool);
+}
+
+#[test]
+fn prop_reverse_no_headers_indexed() {
+    fn p(rows: CsvData) -> bool {
+        prop_reverse_indexed("prop_reverse_no_headers_indexed", rows, false)
+    }
+    qcheck(p as fn(CsvData) -> bool);
+}
