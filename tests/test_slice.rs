0e2fd80d5f7d85d046a15ddb7e8a0d88a30c8017
Joel Natividad <1980690+jqnatividad@users.noreply.github.com>
2024-04-07 14:59:53 -0400

    `tests`: add `--json` tests for `slice`

--- tests/test_slice.rs
+++ tests/test_slice.rs
@@ -10,49 +10,91 @@ macro_rules! slice_tests {
             #[test]
             fn headers_no_index() {
                 let name = concat!(stringify!($name), "headers_no_index");
-                test_slice(name, $start, $end, $expected, true, false, false);
+                test_slice(name, $start, $end, $expected, true, false, false, false);
             }
 
             #[test]
             fn no_headers_no_index() {
                 let name = concat!(stringify!($name), "no_headers_no_index");
-                test_slice(name, $start, $end, $expected, false, false, false);
+                test_slice(name, $start, $end, $expected, false, false, false, false);
+            }
+
+            #[test]
+            fn no_headers_no_index_json() {
+                let name = concat!(stringify!($name), "no_headers_no_index_json");
+                test_slice(name, $start, $end, $expected, false, false, false, true);
             }
 
             #[test]
             fn headers_index() {
                 let name = concat!(stringify!($name), "headers_index");
-                test_slice(name, $start, $end, $expected, true, true, false);
+                test_slice(name, $start, $end, $expected, true, true, false, false);
             }
 
             #[test]
             fn no_headers_index() {
                 let name = concat!(stringify!($name), "no_headers_index");
-                test_slice(name, $start, $end, $expected, false, true, false);
+                test_slice(name, $start, $end, $expected, false, true, false, false);
+            }
+
+            #[test]
+            fn headers_index_json() {
+                let name = concat!(stringify!($name), "headers_index_json");
+                test_slice(name, $start, $end, $expected, true, true, false, true);
+            }
+
+            #[test]
+            fn no_headers_index_json() {
+                let name = concat!(stringify!($name), "no_headers_index_json");
+                test_slice(name, $start, $end, $expected, false, true, false, true);
             }
 
             #[test]
             fn headers_no_index_len() {
                 let name = concat!(stringify!($name), "headers_no_index_len");
-                test_slice(name, $start, $end, $expected, true, false, true);
+                test_slice(name, $start, $end, $expected, true, false, true, false);
             }
 
             #[test]
             fn no_headers_no_index_len() {
                 let name = concat!(stringify!($name), "no_headers_no_index_len");
-                test_slice(name, $start, $end, $expected, false, false, true);
+                test_slice(name, $start, $end, $expected, false, false, true, false);
+            }
+
+            #[test]
+            fn headers_no_index_len_json() {
+                let name = concat!(stringify!($name), "headers_no_index_len_json");
+                test_slice(name, $start, $end, $expected, true, false, true, true);
+            }
+
+            #[test]
+            fn no_headers_no_index_len_json() {
+                let name = concat!(stringify!($name), "no_headers_no_index_len_json");
+                test_slice(name, $start, $end, $expected, false, false, true, true);
             }
 
             #[test]
             fn headers_index_len() {
                 let name = concat!(stringify!($name), "headers_index_len");
-                test_slice(name, $start, $end, $expected, true, true, true);
+                test_slice(name, $start, $end, $expected, true, true, true, false);
             }
 
             #[test]
             fn no_headers_index_len() {
                 let name = concat!(stringify!($name), "no_headers_index_len");
-                test_slice(name, $start, $end, $expected, false, true, true);
+                test_slice(name, $start, $end, $expected, false, true, true, false);
+            }
+
+            #[test]
+            fn headers_index_len_json() {
+                let name = concat!(stringify!($name), "headers_index_len_json");
+                test_slice(name, $start, $end, $expected, true, true, true, true);
+            }
+
+            #[test]
+            fn no_headers_index_len_json() {
+                let name = concat!(stringify!($name), "no_headers_index_len_json");
+                test_slice(name, $start, $end, $expected, false, true, true, true);
             }
         }
     };
@@ -84,6 +126,7 @@ fn test_slice(
     headers: bool,
     use_index: bool,
     as_len: bool,
+    json_output: bool,
 ) {
     let (wrk, mut cmd) = setup(name, headers, use_index);
     if let Some(start) = start {
@@ -105,16 +148,42 @@ fn test_slice(
     if !headers {
         cmd.arg("--no-headers");
     }
+    if json_output {
+        let output_file = wrk.path("output.json").to_string_lossy().to_string();
 
-    let got: Vec<Vec<String>> = wrk.read_stdout(&mut cmd);
-    let mut expected = expected
-        .iter()
-        .map(|&s| vec![s.to_owned()])
-        .collect::<Vec<Vec<String>>>();
-    if headers {
-        expected.insert(0, svec!["header"]);
+        cmd.arg("--json").args(&["--output", &output_file]);
+
+        wrk.assert_success(&mut cmd);
+
+        let gots = wrk.read_to_string(&output_file);
+        let gotj: serde_json::Value = serde_json::from_str(&gots).unwrap();
+        let got = gotj.to_string();
+        // let expected = "".to_string();
+
+        let expected_vec = expected
+            .iter()
+            .map(|&s| {
+                if headers {
+                    format!("{{\"header\":\"{}\"}}", s)
+                } else {
+                    format!("{{\"0\":\"{}\"}}", s)
+                }
+            })
+            .collect::<Vec<String>>();
+        let expected = format!("[{}]", expected_vec.join(","));
+
+        assert_eq!(got, expected);
+    } else {
+        let got: Vec<Vec<String>> = wrk.read_stdout(&mut cmd);
+        let mut expected = expected
+            .iter()
+            .map(|&s| vec![s.to_owned()])
+            .collect::<Vec<Vec<String>>>();
+        if headers {
+            expected.insert(0, svec!["header"]);
+        }
+        assert_eq!(got, expected);
     }
-    assert_eq!(got, expected);
 }
 
 fn test_index(name: &str, idx: isize, expected: &str, headers: bool, use_index: bool) {
@@ -149,6 +218,7 @@ fn slice_negative_with_len() {
         true,
         true,
         true,
+        false,
     );
     test_slice(
         "slice_negative_start_no_headers_index_len",
@@ -158,6 +228,7 @@ fn slice_negative_with_len() {
         false,
         true,
         true,
+        false,
     );
     test_slice(
         "slice_negative_start_headers_no_index_len",
@@ -166,6 +237,41 @@ fn slice_negative_with_len() {
         &["b", "c"],
         true,
         false,
+        true,
+        false,
+    );
+}
+
+#[test]
+fn slice_negative_with_len_json() {
+    test_slice(
+        "slice_negative_start_headers_index_len_json",
+        Some(-4),
+        Some(2),
+        &["b", "c"],
+        true,
+        true,
+        true,
+        true,
+    );
+    test_slice(
+        "slice_negative_start_no_headers_index_len_json",
+        Some(-4),
+        Some(2),
+        &["b", "c"],
+        false,
+        true,
+        true,
+        true,
+    );
+    test_slice(
+        "slice_negative_start_headers_no_index_len_json",
+        Some(-4),
+        Some(2),
+        &["b", "c"],
+        true,
+        false,
+        true,
         true,
     );
 }
