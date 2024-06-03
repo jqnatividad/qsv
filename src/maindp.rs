da879a0ae7bc66a8d86cce774fd8b3eee13e5cf5
rzmk <mueez.khan@proton.me>
2024-06-03 16:57:38 -0400

    `prompt`: add `qsv prompt` to pick a file with a file dialog & write to stdout

--- src/maindp.rs
+++ src/maindp.rs
@@ -69,6 +69,7 @@ macro_rules! command_list {
     input       Read CSVs w/ special quoting, skipping, trimming & transcoding rules
     joinp       Join CSV files using the Pola.rs engine
     luau        Execute Luau script on CSV data
+    prompt      Open a file dialog to pick a file
     pseudo      Pseudonymise the values of a column
     rename      Rename the columns of CSV data efficiently
     replace     Replace patterns in CSV data
@@ -257,6 +258,7 @@ enum Command {
     JoinP,
     #[cfg(feature = "luau")]
     Luau,
+    Prompt,
     Pseudo,
     Rename,
     Replace,
@@ -314,6 +316,7 @@ impl Command {
             Command::JoinP => cmd::joinp::run(argv),
             #[cfg(feature = "luau")]
             Command::Luau => cmd::luau::run(argv),
+            Command::Prompt => cmd::prompt::run(argv),
             Command::Pseudo => cmd::pseudo::run(argv),
             Command::Rename => cmd::rename::run(argv),
             Command::Replace => cmd::replace::run(argv),
